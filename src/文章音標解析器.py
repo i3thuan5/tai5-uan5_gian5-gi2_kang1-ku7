a23400d9050f59eb5e2940ffbd36c6adde256eeb
Ihc <Ihc@gmail.com>
2013-03-03 16:50:06 +0800

    測試通用對照表，順紲修改 丞宏

--- src/文章音標解析器.py
+++ src/文章音標解析器.py
@@ -22,7 +22,13 @@ class 文章音標解析器:
 			for 音標長度 in range(16, 0, -1):
 				音標 = self.音標工具(語句[處理位置:處理位置 + 音標長度])
 				if 音標.音標 != None:
-					解析結果 += 音標.轉換到臺灣閩南語羅馬字拼音()
+					臺灣閩南語羅馬字拼音=音標.轉換到臺灣閩南語羅馬字拼音()
+					if 臺灣閩南語羅馬字拼音==None:
+						print(音標.音標+'轉臺灣閩南語羅馬字拼音有問題！！')				
+						print(音標.聲韻對照表[音標.聲韻])
+						無問題諾 = False
+					else:
+						解析結果 += 臺灣閩南語羅馬字拼音
 					處理位置 += 音標長度
 					break
 			else:
@@ -43,6 +49,7 @@ if __name__ == '__main__':
 	print(解析器.解析語句('tsua̋n-ne tsua̋n'))
 	print(解析器.解析語句('--tsi̍t-kuá --tsit-kuá'))
 	print(解析器.解析語句('--tsi̍t-kuá -@-tsit-kuá'))
+	print(臺灣閩南語羅馬字拼音('nau2').音標)
 	解析器 = 文章音標解析器(通用拼音音標)
 	print(解析器.解析語句('hue2_zit6_e5_sit7_le4'))
 	print(解析器.解析語句('17,"一下子","一時仔","zit6-si5-a4","cit8-si5-a2"'))
@@ -50,3 +57,5 @@ if __name__ == '__main__':
 	print(解析器.解析語句('m33gorh66 ho23 sua12 a44 liam53 mua45mua44 e52 ve45cia11 ziah77 giann53 vor53 qo23hun12zing11 , 	'))
 	print(解析器.解析語句('qin21 a24 dor43 i45ging12 ga23 tau55 cun22 cut76ki34 cia12 tang12a41qua22 gong44 '))
 	print(解析器.解析語句('mui45  ging12ge34  zit68ging12cu33'))
+	print(解析器.解析語句('nau2'))
+	
