dba49d0f408e9205a7a22623e768984110c6de84
Ihc <Ihc@gmail.com>
2013-03-03 14:56:12 +0800

    另外用一類 丞宏

---
+++ src/言語資料庫/公用資料.py
@@ -0,0 +1,12 @@
+'''
+Created on 2013/3/3
+
+@author: Ihc
+'''
+import postgresql
+資料庫連線 = postgresql.open(host = "localhost", port = 5432, user = "Ihc", password = "983781", database = "言語系統")
+文字組合符號 = ','
+會當替換 = '會當替換'
+袂當替換 = '袂當替換'
+義近 = '義近'
+義倒 = '義倒'
