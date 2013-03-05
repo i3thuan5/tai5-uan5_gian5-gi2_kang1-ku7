48b89cd14c03e65a06372cfacab53b3142f3aa91
Ihc <Ihc@gmail.com>
2013-03-05 13:28:29 +0800

    共台語詞典01創好 丞宏

--- src/教育部臺灣閩南語常用詞辭典/資料庫連線.py
+++ src/教育部臺灣閩南語常用詞辭典/資料庫連線.py
@@ -12,7 +12,7 @@ import postgresql
 資料庫連線 = postgresql.open(host = "localhost", port = 5432, user = "Ihc", password = "983781", database = "言語系統")
 
 揣主條目 = 資料庫連線.prepare('SELECT "主編號","屬性","詞目","音讀","方言差" ' +
-    'FROM "教育部臺灣閩南語常用詞辭典"."詞目總檔" WHERE "主編號"=\'66\' ORDER BY "主編號" ASC')
+    'FROM "教育部臺灣閩南語常用詞辭典"."詞目總檔" ORDER BY "主編號" ASC')
 揣義倒詞的詞音 = 資料庫連線.prepare('SELECT "另注音讀" ' +
     'FROM "教育部臺灣閩南語常用詞辭典"."反義詞對應" WHERE "反義詞" = $1 ORDER BY "流水號"')
 揣義近詞的詞音 = 資料庫連線.prepare('SELECT "另注音讀" ' +
@@ -87,7 +87,7 @@ def 加演化(種類, 腔口, 型體, 音標):
     'WHERE "文白俗替" = \'文\' OR "文白俗替" = \'白\' ORDER BY "詞目" ASC, "文白俗替" ASC')
 
 揣釋義 = 資料庫連線.prepare('SELECT "流水號","主編號","義項順序","詞性","釋義" ' +
-    'FROM "教育部臺灣閩南語常用詞辭典"."釋義" WHERE "流水號">8016 ORDER BY "流水號" ASC LIMIT 5')
+    'FROM "教育部臺灣閩南語常用詞辭典"."釋義" WHERE "流水號">8016 ORDER BY "流水號"')
 
 用釋義揣例句 = 資料庫連線.prepare('SELECT "例句","標音","例句翻譯" ' +
     'FROM "教育部臺灣閩南語常用詞辭典"."例句" WHERE "釋義編號"=$1 ORDER BY "流水號" ASC ')
