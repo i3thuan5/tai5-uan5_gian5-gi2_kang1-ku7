d4694d44d18d7481fb0b068ac741af0e13044492
Ihc <ihcaoe@gmail.com>
2013-07-04 16:51:28 +0800

    共剖析模組化,順紲共自設連線用好 丞宏

--- src/剖析相關工具/剖析工具.py
+++ src/剖析相關工具/剖析工具.py
@@ -3,17 +3,14 @@ Created on 2013/3/1
 
 @author: Ihc
 '''
-from subprocess import Popen, PIPE
+from 剖析相關工具.官方剖析工具 import 官方剖析工具
+from 剖析相關工具.自設剖析工具 import 自設剖析工具
 class 剖析工具:
+	官方工具 = 官方剖析工具()
+	自設工具 = 自設剖析工具()
 	def 剖析(self, 愛轉換的字串):
-		找位置 = Popen(['pwd'], stdout = PIPE)
-		for 一逝 in 找位置.stdout:
-			程式所在 = 一逝[:-1].decode("utf-8")
-		程式所在, 資料夾 = 程式所在.rsplit('/', 1)
-		while 資料夾 != 'src':
-			程式所在, 資料夾 = 程式所在.rsplit('/', 1)
-		程序輸出 = Popen(['/bin/bash' , 程式所在 + '/CKIPClient/剖析.sh', 愛轉換的字串 ], stdout = PIPE)
-		return [ 一逝[:-1].decode("utf-8") for 一逝 in 程序輸出.stdout]
+		return self.官方工具.剖析(愛轉換的字串)
+#		return self.自設工具.剖析(愛轉換的字串)
 
 if __name__ == '__main__':
 	工具 = 剖析工具()
---
+++ src/剖析相關工具/自設剖析工具.py
@@ -0,0 +1,16 @@
+import telnetlib
+
+class 自設剖析工具:
+    主機="localhost"
+    連接埠=27015
+    def 剖析(self, 愛轉換的字串):
+        連線 = telnetlib.Telnet(self.主機,self.連接埠)
+        連線.write(愛轉換的字串.encode('big5') )
+        結果=連線.read_all().decode('big5')
+        連線.close()
+        return 結果.replace('\r','').rstrip().split('\n')
+        
+        
+if __name__ == '__main__':
+    工具 = 自設剖析工具()
+    print(工具.剖析('我想吃飯。我想吃很多飯。'))
