e4badd3359d37d29888207fd431362a2196e1375
Ihc <Ihc@gmail.com>
2013-07-25 19:51:55 +0800

    看測資的音節，調整伊拍毋著的音標　丞宏

--- src/毓哲語言辨識/第一步之揣出臺語通用.py
+++ src/毓哲語言辨識/第一步之揣出臺語通用.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
 # 	print(字音對照.轉換到臺灣閩南語羅馬字拼音())
 	lines = [line.strip() for line in open('/home/Ihc/辨識/Syl2Monophone.dic.txt')]
 # 	lines = [line.strip() for line in open('/home/Ihc/處理愛對齊的語料/臺語通用拼音.dic')]
+	對照表={}
 	for line in lines:
 		通用,*音素=line.split()
 		if 通用.endswith('h') or 通用.endswith('p') or 通用.endswith('t') or 通用.endswith('k'):
@@ -16,6 +17,7 @@ if __name__ == '__main__':
 		字音對照 = 通用拼音音標(通用)
 		if 字音對照.轉換到臺灣閩南語羅馬字拼音()!=None or 通用=='sil1':
 			print(line)
+			對照表[通用[:-1]]=音素
 			pass
 		else:
 # 			print(通用[:-1])
@@ -23,3 +25,13 @@ if __name__ == '__main__':
 # 			print(字音對照.轉換到臺灣閩南語羅馬字拼音())
 #			grep -v er xx| grep -v eu | grep -v y | grep -v ei | grep -v ou | grep -v ii | grep -v uo | grep -v ung | grep -v ch | grep -v zh | grep -v sh | grep -v oi | grep -v em | grep -v ep | grep -v r | grep -v uang
 			pass
+# 	語法錯誤表=[('niunn','niu'),('min','bin'),('ian','iam'),('ghian','giam'),('cian','tshiam'),
+# 		('gian','kian'),('hiat','hiak'),('zian','ziam'),('ming','bing'),('mong','bong'),
+# 		('puo','phoo'),('huo','hoo'),('s','si'),('z','tsi')]
+	語法錯誤表=[('niunn','niu'),('min','bin'),('ian','iam'),('ghian','ghiam'),('cian','ciam'),
+		('gian','giam'),('hiat','hiah'),('zian','ziam'),('ming','bing'),('mong','bong'),
+		('puo','po'),('huo','ho'),('s','si'),('z','zi')]
+	for 原本,修正 in 語法錯誤表:
+		print(原本,end=' ')
+		print(' '.join(對照表[修正]))
+																		
