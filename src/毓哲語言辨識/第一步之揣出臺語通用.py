6b0810a7d043a3563d7d5197e0f73fb60da6b43c
Ihc <Ihc@gmail.com>
2013-07-27 06:44:28 +0800

    語音辨識佮合成的程式提來遮處理　丞宏

---
+++ src/毓哲語言辨識/第一步之揣出臺語通用.py
@@ -0,0 +1,37 @@
+from 通用拼音音標 import 通用拼音音標
+
+# 語法錯誤表=[('niunn','niu'),('min','bin'),('ian','iam'),('ghian','giam'),('cian','tshiam'),
+# 	('gian','kian'),('hiat','hiak'),('zian','ziam'),('ming','bing'),('mong','bong'),
+# 	('puo','phoo'),('huo','hoo'),('s','si'),('z','tsi')]
+語法錯誤表=[('niunn','niu'),('min','bin'),('ian','iam'),('ghian','ghiam'),('cian','ciam'),
+	('gian','giam'),('hiat','hiah'),('zian','ziam'),('ming','bing'),('mong','bong'),
+	('puo','po'),('huo','ho'),('s','si'),('z','zi')]
+
+if __name__ == '__main__':
+# 	字音對照 = 通用拼音音標('bai5')
+# 	print(字音對照.音標)
+# 	print(字音對照.轉換到臺灣閩南語羅馬字拼音())
+	lines = [line.strip() for line in open('/home/Ihc/辨識/Syl2Monophone.dic.txt')]
+# 	lines = [line.strip() for line in open('/home/Ihc/處理愛對齊的語料/臺語通用拼音.dic')]
+	對照表={}
+	for line in lines:
+		通用,*音素=line.split()
+		if 通用.endswith('h') or 通用.endswith('p') or 通用.endswith('t') or 通用.endswith('k'):
+			通用+='7'
+		else:
+			通用+='1'
+		字音對照 = 通用拼音音標(通用)
+		if 字音對照.轉換到臺灣閩南語羅馬字拼音()!=None or 通用=='sil1':
+			print(line)
+			對照表[通用[:-1]]=音素
+			pass
+		else:
+# 			print(通用[:-1])
+# 			print(字音對照.音標)
+# 			print(字音對照.轉換到臺灣閩南語羅馬字拼音())
+#			grep -v er xx| grep -v eu | grep -v y | grep -v ei | grep -v ou | grep -v ii | grep -v uo | grep -v ung | grep -v ch | grep -v zh | grep -v sh | grep -v oi | grep -v em | grep -v ep | grep -v r | grep -v uang
+			pass
+	for 原本,修正 in 語法錯誤表:
+		print(原本,end=' ')
+		print(' '.join(對照表[修正]))
+																		
---
+++ src/毓哲語言辨識/第三步之產生臺羅聲韻對照通用音素.py
@@ -0,0 +1,47 @@
+from 通用拼音音標 import 通用拼音音標
+from 臺灣閩南語羅馬字拼音 import 臺灣閩南語羅馬字拼音
+from 毓哲語言辨識.第二步之產生通用點臺羅聲韻對照 import 臺羅聲韻轉辨識合成型
+from 毓哲語言辨識.第一步之揣出臺語通用 import 語法錯誤表
+
+#模型無逐个音素
+音素調整={'unnh':'unn','onnk':'onnh','ngh':'ng','mh':'m','ernnh':'onnh',}
+
+
+if __name__ == '__main__':
+# 	字音對照 = 通用拼音音標('bai5')
+# 	print(字音對照.音標)
+# 	print(字音對照.轉換到臺灣閩南語羅馬字拼音())
+# 	lines = [line.strip() for line in open('/home/Ihc/辨識/Syl2Monophone.dic.txt')]
+	錯誤對照表={}
+	for 原本,修正 in 語法錯誤表:
+		錯誤對照表[原本]=修正
+		
+	lines = [line.strip() for line in open('/home/Ihc/處理愛對齊的語料/臺語通用拼音.dic')]
+	對照表={}
+	for line in lines:
+		if line=='':
+			continue
+		通用,*音素=line.split()
+		if 通用 in 錯誤對照表:
+			通用=錯誤對照表[通用]
+		if 通用.endswith('h') or 通用.endswith('p') or 通用.endswith('t') or 通用.endswith('k'):
+			通用+='7'
+		else:
+			通用+='1'
+		字音對照 = 通用拼音音標(通用)
+		if 通用=='sil1':
+				對照表['sil']='sil'
+		else:
+			臺羅拼音=臺灣閩南語羅馬字拼音(字音對照.轉換到臺灣閩南語羅馬字拼音())
+			聲,韻=臺羅聲韻轉辨識合成型(臺羅拼音.聲,臺羅拼音.韻)
+			for i in range(len(音素)):
+				if 音素[i] in 音素調整:
+					音素[i]=音素調整[音素[i]]
+			if 聲=='':
+				對照表[韻]=' '.join(音素)
+			else:
+				對照表[聲]=音素[0]
+				對照表[韻]=' '.join(音素[1:])
+	for 對照 in 對照表.items():
+		print(' '.join(對照))
+				
