e3c41db0d1e079a4b9173b18399e807ffd1b27ba
Ihc <ihcaoe@gmail.com>
2013-08-01 13:37:11 +0800

    詞條佮音標處理　丞宏

--- src/教育部臺灣客家語常用詞辭典/客語辭典剖析結果處理.py
+++ src/教育部臺灣客家語常用詞辭典/客語辭典剖析結果處理.py
@@ -4,6 +4,8 @@ Created on 2013/7/31
 @author: chhsueh
 '''
 from 教育部臺灣客家語常用詞辭典.客話辭典網頁剖析工具 import 客話辭典網頁剖析工具
+import os
+from curses.ascii import isalpha, isdigit
 
 class 客語辭典剖析結果處理():
     剖析結果=[]
@@ -18,29 +20,52 @@ class 客語辭典剖析結果處理():
             詞性=剖析結果[2].split(':')[1].strip()
         else:
             詞性=''
-        return [剖析結果[1],詞性,剖析結果[5],剖析結果[8],剖析結果[11],剖析結果[14],剖析結果[17],
+        return [剖析結果[1][1:-1],詞性,self.分開音標(剖析結果[5]),self.分開音標(剖析結果[8]),
+                self.分開音標(剖析結果[11]),self.分開音標(剖析結果[14]),self.分開音標(剖析結果[17]),
                 剖析結果[20],剖析結果[22],剖析結果[24],剖析結果[26],剖析結果[28],剖析結果[30],剖析結果[32], ]
     def 轉資料庫格式(self,剖析結果):
         return '("'+'","'.join(self.標籤名())+"\") VALUES ('"+"','".join(self.欄位值(剖析結果))+"')"
+    def 分開音標(self,連做伙音標):
+        頂一个音=None
+        音標結果=''
+        for 音 in 連做伙音標:
+            if 頂一个音!=None and isalpha(音) and isdigit(頂一个音):
+                音標結果+=' '
+            音標結果+=音
+            頂一个音=音
+        return 音標結果
+            
+        
 
 
 if __name__ == "__main__":
     網頁剖析工具 = 客話辭典網頁剖析工具(strict=False)
-    su=open('/home/chhsueh/su.html').read()
-#     print(su)
-    a=網頁剖析工具.剖析客話辭典網頁(su)
-    print(a)
-    word=open('/home/chhsueh/word.html').read()
-#     print(word)
-    a=網頁剖析工具.剖析客話辭典網頁(word)
-    print(a)
-    
     客語辭典剖析結果=客語辭典剖析結果處理()
-    客話辭典資料=網頁剖析工具.剖析客話辭典檔案('/home/chhsueh/su2.html')
-    print(客話辭典資料)
-    b=網頁剖析工具.剖析客話辭典檔案('/home/chhsueh/word2.html')
-    print(b)
-    print(客語辭典剖析結果.標籤名())
-    print(客語辭典剖析結果.欄位值(客話辭典資料))
-    print(客語辭典剖析結果.轉資料庫格式( 客話辭典資料))
-    print(客語辭典剖析結果.轉資料庫格式( b))
+#    su=open('/home/chhsueh/su.html').read()
+##     print(su)
+#    a=網頁剖析工具.剖析客話辭典網頁(su)
+#    print(a)
+#    word=open('/home/chhsueh/word.html').read()
+#     print(word)
+#    a=網頁剖析工具.剖析客話辭典網頁(word)
+#    print(a)
+    os.chdir('/home/ihc/桌面/1020801/hakka/')
+    a=set()
+    b=set()
+    for 檔名 in os.listdir(".")[:10]:
+        if 檔名.startswith("result"):
+#            print(檔名)
+            客話辭典資料=網頁剖析工具.剖析客話辭典檔案(檔名)
+            資料庫指令=客語辭典剖析結果.轉資料庫格式(客話辭典資料)
+            print(資料庫指令)
+#            a.add(客語辭典剖析結果.欄位值(客話辭典資料)[0][0])
+#            b.add(客語辭典剖析結果.欄位值(客話辭典資料)[0][-1])
+            
+#    客話辭典資料=網頁剖析工具.剖析客話辭典檔案('/home/chhsueh/su2.html')
+#    print(客話辭典資料)
+#    b=網頁剖析工具.剖析客話辭典檔案('/home/chhsueh/word2.html')
+#    print(b)
+#    print(客語辭典剖析結果.標籤名())
+#    print(客語辭典剖析結果.欄位值(客話辭典資料))
+#    print(客語辭典剖析結果.轉資料庫格式(客話辭典資料))
+#    print(客語辭典剖析結果.轉資料庫格式(b))
