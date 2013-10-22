541e209da0e1393d9878f154b1c4d46610d42d21
Ihc <ihcaoe@gmail.com>
2013-10-22 20:21:24 +0800

    加整合，等其他匯好才閣匯　丞宏

--- src/資料佮語料匯入整合/新酷音輸入法/匯入資料庫.py
+++ src/資料佮語料匯入整合/新酷音輸入法/匯入資料庫.py
@@ -3,10 +3,10 @@ from 資料佮語料匯入整合.新酷音輸入法.詞庫整理 import 整理�
 from 資料庫.資料庫連線 import 資料庫連線
 
 
-資料庫指令 = 資料庫連線.prepare('INSERT INTO "新酷音輸入法"."字詞" ' + 
-    '("字","音") ' + 'VALUES ($1,$2) ')
 
 if __name__ == "__main__":
+    資料庫指令 = 資料庫連線.prepare('INSERT INTO "新酷音輸入法"."字詞" ' + 
+    '("字","音") ' + 'VALUES ($1,$2) ')
     接表 = list(整理字表())
     接表.extend(整理詞庫())
     合表 = set(接表)
