c325f3e2f2125151ca8bd1173a3002e31fa76763
Ihc <ihcaoe@gmail.com>
2013-10-22 20:44:14 +0800

    處理集的輸出先後　丞宏

---
+++ 測試/單一功能/字詞組集句章/解析整理工具/集內組照排測試.py
@@ -0,0 +1,14 @@
+from unittest.case import TestCase
+
+class 集內組照排測試(TestCase):
+	def setUp(self):
+		pass
+
+	def tearDown(self):
+		pass
+
+	def test_文白照排(self):
+		self.assertEqual(0,1)
+
+	def test_型照排(self):
+		self.assertEqual(0,1)
