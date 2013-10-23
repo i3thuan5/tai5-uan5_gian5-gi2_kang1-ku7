10258149306e25d00d907285eaa422bbaf0be788
Ihc <Ihc@gmail.com>
2013-10-23 23:51:12 +0800

    改條目格式會當順利整合　丞宏

--- src/字詞組集句章/音標系統/閩南語/通用拼音音標.py
+++ src/字詞組集句章/音標系統/閩南語/通用拼音音標.py
@@ -24,37 +24,37 @@ from 字詞組集句章.音標系統.閩南語.臺灣閩南語羅馬字拼音 im
 '':'',
 }
 通用拼音佮臺灣羅馬韻母對照表 = {
-	'a':'a','ah':'ah','ap':'ap','at':'at','ak':'ak',
-	'am':'am','an':'an','ang':'ang',
-	'ann':'ann','annh':'annh',
-	'ai':'ai','aih':'aih','ainn':'ainn','ainnh':'ainnh',
-	'au':'au','auh':'auh','aunn':'aunn','aunnh':'aunnh',
-	'e':'e','eh':'eh','en':'ian','et':'iat','enn':'enn','ennh':'ennh',
-	'er':'o','erh':'oh','ernn':'onn',
-	'ia':'ia','iah':'iah','iap':'iap','iak':'iak',
-	'iam':'iam','iang':'iang',
-	'iann':'iann','iannh':'iannh',
-	'iau':'iau','iauh':'iauh','iaunn':'iaunn','iaunnh':'iaunnh',
-	'ier':'io','ierh':'ioh',
-	'i':'i','ih':'ih','ip':'ip','it':'it','ik':'ik',
-	'im':'im','in':'in','ing':'ing',
-	'inn':'inn','innh':'innh',
-	'io':'ioo','ioh':'iooh','iok':'iok','iong':'iong','ionn':'ionn',
-	'ior':'io','iorh':'ioh',
-	'iu':'iu','iuh':'iuh','iut':'iut','iunn':'iunn','iunnh':'iunnh',
-	'm':'m','mh':'mh','ng':'ng','ngh':'ngh',
-	'o':'oo','oh':'ooh','op':'op','ok':'ok',
-	'om':'om','ong':'ong',
-	'onn':'onn','onnh':'onnh',
-	'or':'o','orh':'oh','orm':'om','ornn':'onn',
-	'ua':'ua','uah':'uah','uat':'uat','uak':'uak','uainn':'uainn','uainnh':'uainnh',
-	'uan':'uan','uann':'uann',
-	'uai':'uai','uaih':'uaih',
-	'ue':'ue','ueh':'ueh','uenn':'uenn','uennh':'uennh',
+	'a':'a', 'ah':'ah', 'ap':'ap', 'at':'at', 'ak':'ak',
+	'am':'am', 'an':'an', 'ang':'ang',
+	'ann':'ann', 'annh':'annh',
+	'ai':'ai', 'aih':'aih', 'ainn':'ainn', 'ainnh':'ainnh',
+	'au':'au', 'auh':'auh', 'aunn':'aunn', 'aunnh':'aunnh',
+	'e':'e', 'eh':'eh', 'en':'ian', 'et':'iat', 'enn':'enn', 'ennh':'ennh',
+	'er':'o', 'erh':'oh', 'ernn':'onn',
+	'ia':'ia', 'iah':'iah', 'iap':'iap', 'iak':'iak',
+	'iam':'iam', 'iang':'iang',
+	'iann':'iann', 'iannh':'iannh',
+	'iau':'iau', 'iauh':'iauh', 'iaunn':'iaunn', 'iaunnh':'iaunnh',
+	'ier':'io', 'ierh':'ioh',
+	'i':'i', 'ih':'ih', 'ip':'ip', 'it':'it', 'ik':'ik',
+	'im':'im', 'in':'in', 'ing':'ing',
+	'inn':'inn', 'innh':'innh',
+	'io':'ioo', 'ioh':'iooh', 'iok':'iok', 'iong':'iong', 'ionn':'ionn',
+	'ior':'io', 'iorh':'ioh',
+	'iu':'iu', 'iuh':'iuh', 'iut':'iut', 'iunn':'iunn', 'iunnh':'iunnh',
+	'm':'m', 'mh':'mh', 'ng':'ng', 'ngh':'ngh',
+	'o':'oo', 'oh':'ooh', 'op':'op', 'ok':'ok',
+	'om':'om', 'ong':'ong',
+	'onn':'onn', 'onnh':'onnh',
+	'or':'o', 'orh':'oh', 'orm':'om', 'ornn':'onn',
+	'ua':'ua', 'uah':'uah', 'uat':'uat', 'uak':'uak', 'uainn':'uainn', 'uainnh':'uainnh',
+	'uan':'uan', 'uann':'uann', 'uannh':'uannh',
+	'uai':'uai', 'uaih':'uaih',
+	'ue':'ue', 'ueh':'ueh', 'uenn':'uenn', 'uennh':'uennh',
 	'ui':'ui',
-	'uih':'uih','uinn':'uinn','uinnh':'uinnh',
-	'u':'u','un':'un','uh':'uh','ut':'ut',
-	'ie':'ie','uang':'uang',
+	'uih':'uih', 'uinn':'uinn', 'uinnh':'uinnh',
+	'u':'u', 'un':'un', 'uh':'uh', 'ut':'ut',
+	'ie':'ie', 'uang':'uang',
 	}
 通用拼音佮臺灣羅馬調類對照表 = {
 	'1':'1', '2':'7', '3':'3', '4':'2', '5':'5',
@@ -81,27 +81,27 @@ class 通用拼音音標(閩南語音標介面):
 					self.調 = 音標[-1:]
 					if self.韻.endswith('h') or self.韻.endswith('p') or \
 						self.韻.endswith('t') or self.韻.endswith('k'):
-						if self.調=='1':
-							self.調='6'
-						elif self.調=='2':
-							self.調='7'
-						elif self.調=='3':
-							self.調='8'
+						if self.調 == '1':
+							self.調 = '6'
+						elif self.調 == '2':
+							self.調 = '7'
+						elif self.調 == '3':
+							self.調 = '8'
 					self.聲韻 = self.聲 + self.韻
 					self.音標 = self.聲韻 + self.調
-		if self.轉換到臺灣閩南語羅馬字拼音()==None:
+		if self.轉換到臺灣閩南語羅馬字拼音() == None:
 			self.聲韻 = None
 			self.音標 = None
-		
+
 	def 轉換到臺灣閩南語羅馬字拼音(self):
 		if self.音標 == None:
 			return None
 		聲 = self.聲母對照表[self.聲]
 		韻 = self.韻母對照表[self.韻]
 		調 = self.調類對照表[self.調]
-		臺羅=臺灣閩南語羅馬字拼音(聲 + 韻 + 調)
+		臺羅 = 臺灣閩南語羅馬字拼音(聲 + 韻 + 調)
 		return 臺羅.音標
-	
+
 # if __name__ == '__main__':
 # 	for 通, 臺 in 通用拼音佮臺灣羅馬韻母對照表.items():
 # 		print("'{0}':'{1}',".format(臺,通))
