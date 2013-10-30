de71c1ce10f4cec7886e303db39ac52b51174316
Ihc <ihcaoe@gmail.com>
2013-10-30 09:25:33 +0800

    會當看資料產生標仔　丞宏

--- src/服務架設/語音合成服務.py
+++ src/服務架設/語音合成服務.py
@@ -3,9 +3,13 @@ from http.server import HTTPServer
 from 服務架設.連線控制器 import 連線控制器
 from 資料庫.欄位資訊 import 偏漳優勢音腔口
 from 斷詞標音.自動標音 import 自動標音
+from 語音合成.合音檔.句物件轉合成標籤 import 句物件轉合成標籤
+from 字詞組集句章.基本元素.集 import 集
+from 字詞組集句章.基本元素.句 import 句
 
 class 語音合成服務(連線控制器):
 	標音工具 = 自動標音()
+	合成標籤工具 = 句物件轉合成標籤()
 	def do_GET(self):
 		try:
 			self.送出連線成功資訊()
@@ -23,8 +27,21 @@ class 語音合成服務(連線控制器):
 			if 查詢腔口 not in self.標音工具.支援腔口:
 				查詢腔口 = 偏漳優勢音腔口
 				查詢語句 = 查詢字串
-			章物件=self.標音工具.標音(查詢腔口, 查詢語句)
-# 			self.輸出(self.標音工具.標音(查詢腔口, 查詢語句))
+			章物件=self.標音工具.斷詞標音物件(查詢腔口, 查詢語句)
+			所在=0
+			集陣列=[]
+			for 句物件 in 章物件.內底句:
+				for 集物件 in 句物件.內底集:
+					if 所在<len(集選擇):
+						選擇=集選擇[所在]
+					else:
+						選擇=0
+					if 選擇<len(集物件.內底組):
+						組陣列=集物件.內底組[選擇:選擇+1]
+					else:
+						組陣列=集物件.內底組[:1]
+					集陣列.append(集(組陣列))
+			self.輸出(self.合成標籤工具.句物件轉標籤(句(集陣列)))
 			return
 		except IOError:
 			self.send_error(404, 'File Not Found: %s' % self.path)
