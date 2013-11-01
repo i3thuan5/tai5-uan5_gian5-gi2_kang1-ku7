38bd501e84adcb68acda270b45a34aa24c8a691f
Ihc <ihcaoe@gmail.com>
2013-11-01 15:00:08 +0800

    改變函式寫法　丞宏

--- src/斷詞標音/排標音結果.py
+++ src/斷詞標音/排標音結果.py
@@ -6,20 +6,20 @@ class 排標音結果:
 	條目 = 辭典條目()
 	文讀層 = None
 	白話層 = None
-	白文照排 = None
 	def __init__(self):
-		self.文讀層=self.條目.文讀層
-		self.白話層=self.條目.白話層
-		def 白文照排函式(組物件):
-			詞物件=組物件.內底詞[0]
-			白=0
-			文=0
-			if hasattr(詞物件, '屬性'):
-				if self.白話層 in 詞物件.屬性:
-					白=-詞物件.屬性[self.白話層]
-				if self.文讀層 in 詞物件.屬性:
-					文=詞物件.屬性[self.文讀層]
-			return (白,文)
-		self.白文照排 = 白文照排函式
+		self.文讀層 = self.條目.文讀層
+		self.白話層 = self.條目.白話層
+
 	def 照白文層排(self, 物件):
 		return self.組照排.排好(self.白文照排, 物件)
+
+	def 白文照排(self, 組物件):
+		詞物件 = 組物件.內底詞[0]
+		白 = 0
+		文 = 0
+		if hasattr(詞物件, '屬性'):
+			if self.白話層 in 詞物件.屬性:
+				白 = -詞物件.屬性[self.白話層]
+			if self.文讀層 in 詞物件.屬性:
+				文 = 詞物件.屬性[self.文讀層]
+		return (白, 文)
