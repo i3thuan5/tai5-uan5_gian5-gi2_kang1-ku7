ffecd9259819f7c03c06cbe716d2e11cb7a0c480
Ihc <ihcaoe@gmail.com>
2013-11-01 15:52:54 +0800

    寫好測試順紲改程式　丞宏

--- src/字詞組集句章/解析整理工具/集內組照排.py
+++ src/字詞組集句章/解析整理工具/集內組照排.py
@@ -7,11 +7,11 @@ from 字詞組集句章.解析整理工具.型態錯誤 import 型態錯誤
 class 集內組照排:
     def 排好(self, 排法, 物件):
         if isinstance(物件, 集):
-            return self.排好集物件(排法,物件)
+            return self.排好集物件(排法, 物件)
         if isinstance(物件, 句):
-            return self.排好句物件(排法,物件)
+            return self.排好句物件(排法, 物件)
         if isinstance(物件, 章):
-            return self.排好章物件(排法,物件)
+            return self.排好章物件(排法, 物件)
         raise 型態錯誤('傳入來的毋是集句章其中一種物件：{0}，{1}'
             .format(type(物件), str(物件)))
         
@@ -19,7 +19,9 @@ class 集內組照排:
         if not isinstance(集物件, 集):
             raise 型態錯誤('傳入來的毋是集物件：{0},{1}'
                 .format(type(集物件), str(集物件)))
-        return 集(sorted(集物件.內底組, key=排法))
+        排好集 = 集()
+        排好集.內底組 = sorted(集物件.內底組, key=排法)
+        return 排好集
         
     def 排好句物件(self, 排法, 句物件):
         if not isinstance(句物件, 句):
@@ -27,8 +29,10 @@ class 集內組照排:
                 .format(type(句物件), str(句物件)))
         集陣列 = []
         for 一集 in 句物件.內底集:
-            集陣列.append(self.排好集物件(排法,一集))
-        return 句(集陣列)
+            集陣列.append(self.排好集物件(排法, 一集))
+        排好句 = 句()
+        排好句.內底集 = 集陣列
+        return 排好句
     
     def 排好章物件(self, 排法, 章物件):
         if not isinstance(章物件, 章):
@@ -36,6 +40,8 @@ class 集內組照排:
                 .format(type(章物件), str(章物件)))
         句陣列 = []
         for 一句 in 章物件.內底句:
-            句陣列.append(self.排好句物件(排法,一句))
-        return 章(句陣列)
+            句陣列.append(self.排好句物件(排法, 一句))
+        排好章 = 章()
+        排好章.內底句 = 句陣列
+        return 排好章
 
--- 測試/單一功能/字詞組集句章/解析整理工具/集內組照排測試.py
+++ 測試/單一功能/字詞組集句章/解析整理工具/集內組照排測試.py
@@ -2,45 +2,99 @@ from unittest.case import TestCase
 from 字詞組集句章.解析整理工具.拆文分析器 import 拆文分析器
 from 斷詞標音.排標音結果 import 排標音結果
 from 字詞組集句章.基本元素.集 import 集
+from 字詞組集句章.基本元素.句 import 句
+from 字詞組集句章.基本元素.章 import 章
 from 字詞組集句章.解析整理工具.集內組照排 import 集內組照排
 from 字詞組集句章.解析整理工具.物件譀鏡 import 物件譀鏡
+from timeit import itertools
 
 class 集內組照排測試(TestCase):
 	def setUp(self):
 		self.分析器 = 拆文分析器()
-		self.組照排=集內組照排()
+		self.組照排 = 集內組照排()
 
 	def tearDown(self):
 		pass
 
+	def test_型照排(self):
+		一 = self.分析器.建立組物件('1一')
+		一.內底詞[0].屬性 = '我是一'
+		空 = self.分析器.建立組物件('0零')
+		空.屬性 = '⿰厓係零'
+		顛倒集 = 集()
+		顛倒集.內底組 = [一, 空]
+		顛倒集.資料 = '9'
+		答案集 = 集()
+		答案集.內底組 = [空, 一]
+		顛倒集.物件 = '6'
+		譀鏡 = 物件譀鏡()
+		排法 = lambda 組物件:譀鏡.看型(組物件)
+		self.assertEqual(self.組照排.排好(排法, 顛倒集), 答案集)
+		self.assertEqual(self.組照排.排好(排法, 答案集), 答案集)
+		self.assertEqual(self.組照排.排好(排法, 顛倒集).內底組[0].屬性, 空.屬性)
+		self.assertEqual(self.組照排.排好(排法, 答案集).內底組[0].屬性, 空.屬性)
+		self.assertEqual(self.組照排.排好(排法, 顛倒集).內底組[1].內底詞[0].屬性, 一.內底詞[0].屬性)
+		self.assertEqual(self.組照排.排好(排法, 答案集).內底組[1].內底詞[0].屬性, 一.內底詞[0].屬性)
+		self.assertEqual(hasattr(self.組照排.排好(排法, 顛倒集), '資料'), False)
+		self.assertEqual(hasattr(self.組照排.排好(排法, 答案集), '物件'), False)
+		句物件 = 句()
+		句物件.內底集 = [答案集, 顛倒集, 顛倒集]
+		self.assertEqual(self.組照排.排好(排法, 句物件), 句([答案集, 答案集, 答案集]))
+		self.assertEqual(self.組照排.排好(排法, 句物件).內底集[0].內底組[0].屬性, 空.屬性)
+		self.assertEqual(self.組照排.排好(排法, 句物件).內底集[0].內底組[1].內底詞[0].屬性, 一.內底詞[0].屬性)
+		self.assertEqual(self.組照排.排好(排法, 句物件).內底集[1].內底組[0].屬性, 空.屬性)
+		self.assertEqual(self.組照排.排好(排法, 句物件).內底集[1].內底組[1].內底詞[0].屬性, 一.內底詞[0].屬性)
+		self.assertEqual(self.組照排.排好(排法, 句物件).內底集[2].內底組[0].屬性, 空.屬性)
+		self.assertEqual(self.組照排.排好(排法, 句物件).內底集[2].內底組[1].內底詞[0].屬性, 一.內底詞[0].屬性)
+		章物件 = 章()
+		章物件.內底句 = [句物件, 句物件]
+		self.assertEqual(self.組照排.排好(排法, 章物件),
+			章([句([答案集, 答案集, 答案集]), 句([答案集, 答案集, 答案集])]))
+		self.assertEqual(self.組照排.排好(排法, 章物件).內底句[0].內底集[0].內底組[0].屬性, 空.屬性)
+		self.assertEqual(self.組照排.排好(排法, 章物件).內底句[0].內底集[0].內底組[1].內底詞[0].屬性, 一.內底詞[0].屬性)
+		self.assertEqual(self.組照排.排好(排法, 章物件).內底句[0].內底集[1].內底組[0].屬性, 空.屬性)
+		self.assertEqual(self.組照排.排好(排法, 章物件).內底句[0].內底集[1].內底組[1].內底詞[0].屬性, 一.內底詞[0].屬性)
+		self.assertEqual(self.組照排.排好(排法, 章物件).內底句[0].內底集[2].內底組[0].屬性, 空.屬性)
+		self.assertEqual(self.組照排.排好(排法, 章物件).內底句[0].內底集[2].內底組[1].內底詞[0].屬性, 一.內底詞[0].屬性)
+		self.assertEqual(self.組照排.排好(排法, 章物件).內底句[1].內底集[0].內底組[0].屬性, 空.屬性)
+		self.assertEqual(self.組照排.排好(排法, 章物件).內底句[1].內底集[0].內底組[1].內底詞[0].屬性, 一.內底詞[0].屬性)
+		self.assertEqual(self.組照排.排好(排法, 章物件).內底句[1].內底集[1].內底組[0].屬性, 空.屬性)
+		self.assertEqual(self.組照排.排好(排法, 章物件).內底句[1].內底集[1].內底組[1].內底詞[0].屬性, 一.內底詞[0].屬性)
+		self.assertEqual(self.組照排.排好(排法, 章物件).內底句[1].內底集[2].內底組[0].屬性, 空.屬性)
+		self.assertEqual(self.組照排.排好(排法, 章物件).內底句[1].內底集[2].內底組[1].內底詞[0].屬性, 一.內底詞[0].屬性)
+		
+		
 	def test_白文照排(self):
 		排標音 = 排標音結果()
 		白詞 = self.分析器.建立詞物件('泅水')
 		白詞.屬性 = {排標音.白話層:1, 排標音.文讀層:0}
-		白組= self.分析器.建立組物件('')
+		白組 = self.分析器.建立組物件('')
 		白組.內底詞.append(白詞)
 		文詞 = self.分析器.建立詞物件('游泳')
 		文詞.屬性 = {排標音.白話層:0, 排標音.文讀層:1}
-		文組= self.分析器.建立組物件('')
+		文組 = self.分析器.建立組物件('')
 		文組.內底詞.append(文詞)
-		顛倒集=集()
-		顛倒集.內底組=[文組,白組]
-		答案集=集()
-		答案集.內底組=[白組,文組]
+		顛倒集 = 集()
+		顛倒集.內底組 = [文組, 白組]
+		答案集 = 集()
+		答案集.內底組 = [白組, 文組]
 		self.assertEqual(排標音.照白文層排(顛倒集), 答案集)
 		self.assertEqual(排標音.照白文層排(答案集), 答案集)
-
-	def test_型照排(self):
-		顛倒集=集([self.分析器.建立組物件('1一'),self.分析器.建立組物件('0零')])
-		答案集=集([self.分析器.建立組物件('0零'),self.分析器.建立組物件('1一')])
-		譀鏡=物件譀鏡()
-		排法=lambda 組物件:譀鏡.看型(組物件)
-		self.assertEqual(self.組照排.排好(排法, 顛倒集), 答案集)
-		self.assertEqual(self.組照排.排好(排法, 答案集), 答案集)
 		
-	def test_排句(self):
-		self.assertEqual(0, 1)
-	def test_排章(self):
-		self.assertEqual(0, 1)
-	def test_排章多句多集(self):
-		self.assertEqual(0, 1)
+	def test_排序測試(self):
+		毋著四 = self.分析器.產生對齊組('我有一張椅仔', 'ngoo2 iu2 it4 tiong1 i2 a2')
+		毋著三 = self.分析器.產生對齊組('我有一張椅仔', 'ngoo2 u7 it4 tiong1 i2 a2')
+		毋著二 = self.分析器.產生對齊組('我有一張椅仔', 'ngoo2 u7 tsit8 tiong1 i2 a2')
+		毋著一 = self.分析器.產生對齊組('我有一張椅仔', 'ngoo2 u7 tsit8 tiunn1 i2 a2')
+		無毋著 = self.分析器.產生對齊組('我有一張椅仔', 'gua2 u7 tsit8 tiunn1 i2 a2')
+		無毋著.分數 = 90
+		毋著一.分數 = 80
+		毋著二.分數 = 70
+		毋著三.分數 = 60
+		毋著四.分數 = 50
+		答案集 = 集()
+		答案集.內底組 = [無毋著, 毋著一, 毋著二, 毋著三, 毋著四, ]
+		for 組陣列 in itertools.permutations([毋著四, 毋著三, 毋著二, 毋著一, 無毋著]):
+			集物件 = 集()
+			集物件.內底組 = 組陣列
+			self.assertEqual(self.組照排.排好(lambda 組物件:-組物件.分數, 集物件), 答案集)
