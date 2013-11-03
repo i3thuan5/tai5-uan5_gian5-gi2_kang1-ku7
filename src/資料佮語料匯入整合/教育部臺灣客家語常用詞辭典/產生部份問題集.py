ca4593d3b919b8b1c15bc367da1e7988b4ac4465
Ihc <ihcaoe@gmail.com>
2013-11-03 22:03:00 +0800

    我老公很厲害的加調了～～！～

--- src/資料佮語料匯入整合/教育部臺灣客家語常用詞辭典/產生部份問題集.py
+++ src/資料佮語料匯入整合/教育部臺灣客家語常用詞辭典/產生部份問題集.py
@@ -11,7 +11,7 @@ import itertools
 if __name__ == '__main__':
 # 	音表 = [line.strip().split()
 # 		for line in open(對齊語料路徑 + '臺羅聲韻查通用音素.dic')]
-	音表=[]
+	音表 = []
 	for i in 臺灣客家話拼音聲母對照表:
 		if i not in 臺灣閩南語羅馬字拼音聲母表:
 			音表.append(i)
@@ -19,19 +19,19 @@ if __name__ == '__main__':
 		if i not in 臺灣閩南語羅馬字拼音韻母表:
 			音表.append(i)
 	
-	for 音 in 音表[:]:
-		問題 = 'QS "Si7_' + 音 + '" {*-' + 音 + '+*}'
-		print(問題)
-	
-	for 音 in 音表[:]:
-		問題 = 'QS "Si7_Tau5_' + 音 + '" {' + 音 + '-*}'
-		print(問題)
-	
-	for 音 in 音表[:]:
-		問題 = 'QS "Si7_Bue2_' + 音+ '" {*+' + 音 + '/*}'
-		print(問題)
-	
-	問題版='QS "Si7_Bue2_{0}" {*+{1}/*}'	
-	for 長度 in range(1,len(臺灣客家話拼音調類對照表)+1):
-		for 組合 in itertools.combinations(臺灣客家話拼音調類對照表,長度):
-			print(組合)
+	聲韻問題版 = [('QS "Si7_{0}" {{ {1} }}', '*-{0}+*'),
+		('QS "Si7_Tau5_{0}" {{ {1} }}', '{0}-*'),
+		('QS "Si7_Bue2_{0}" {{ {1} }}', '*+{0}/*')]
+	for 主問題,小問題 in 聲韻問題版:
+		for 音 in 音表[:]:
+			print(主問題.format(音,小問題.format(音)))
+			
+	聲韻問題版 = [('QS "Si7_Tshu2_Tiau3_{0}" {{ {1} }}', '*<{0}>*'),
+		('QS "Si7_Tau5_Tiau3_{0}" {{ {1} }}', '*tiau3:{0}<*'),
+		('QS "Si7_Bue2_Tiau3_{0}" {{ {1} }}', '*>{0}/*')]
+		
+	for 主問題,小問題 in 聲韻問題版:
+		for 長度 in range(1, len(臺灣客家話拼音調類對照表) + 1):
+			for 組合 in itertools.combinations(臺灣客家話拼音調類對照表, 長度):
+				名=str(組合).replace(' ','')
+				print(主問題.format(名,','.join(map(小問題.format,組合))))
