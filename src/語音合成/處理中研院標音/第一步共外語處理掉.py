cd8805f3c4ba7c3cc1eadc7f1b0db63d49596177
chhsueh <ihcaoe@gmail.com>
2013-11-06 17:06:22 +0800

    處理空的逝 丞宏

--- src/語音合成/處理中研院標音/第一步共外語處理掉.py
+++ src/語音合成/處理中研院標音/第一步共外語處理掉.py
@@ -4,12 +4,15 @@ Created on 2013/10/12
 @author: Ihc
 '''
 class 第一步共外語處理掉:
-	def 擲掉外語(self,句集):
+	def 擲掉外語佮空逝(self,句集):
 		資料=[]
 		頂一逝是標籤=True
 		有外語=False
 		for 句 in 句集:
-			if 'language' in 句 or '[//]' in 句 or '_' in 句:
+			句=句.strip()
+			if 句=='':
+				continue
+			elif 'language' in 句 or '[//]' in 句 or '_' in 句:
 				if not 頂一逝是標籤:
 					資料=資料[:-1]
 				有外語=True
