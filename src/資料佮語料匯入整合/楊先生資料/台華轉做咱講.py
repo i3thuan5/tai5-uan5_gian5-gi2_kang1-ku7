f20c83a3cddb4109c7d306ac15509aea4d02e8b7
Ihc <ihcaoe@gmail.com>
2014-01-03 18:10:12 +0800

    變做模組　丞宏

--- src/資料佮語料匯入整合/楊先生資料/台華轉做咱講.py
+++ src/資料佮語料匯入整合/楊先生資料/台華轉做咱講.py
@@ -16,33 +16,28 @@
 感謝您的使用與推廣～～勞力！承蒙！
 """
 from 資料庫.資料庫連線 import 資料庫連線
-from 資料庫.欄位資訊 import 字詞
-from 資料庫.整合.整合入言語 import 加文字佮版本
-from 資料庫.整合.整合入言語 import 加關係
-from 資料庫.欄位資訊 import 義近
-from 資料庫.欄位資訊 import 會當替換
-from 資料庫.整合.教育部閩南語常用詞辭典 import 設定詞性
-from 資料庫.欄位資訊 import 臺員
 from 字詞組集句章.解析整理工具.文章初胚工具 import 文章初胚工具
 from 字詞組集句章.解析整理工具.拆文分析器 import 拆文分析器
 from 字詞組集句章.解析整理工具.轉物件音家私 import 轉物件音家私
 from 字詞組集句章.解析整理工具.物件譀鏡 import 物件譀鏡
 from 字詞組集句章.音標系統.閩南語.教會羅馬字音標 import 教會羅馬字音標
-import hashlib
+from 資料佮語料匯入整合.咱講.轉做咱講工具 import 轉做咱講工具
 
 class 整合台華:
 	揣台華資料 = 資料庫連線.prepare('SELECT "ID","台語羅馬字","台語羅馬字2","台語漢字","華語對譯","英文","pos_h" ' + 
 		'FROM "楊允言先生資料"."台華" ORDER BY "ID"')
 	揣台華資料 = 資料庫連線.prepare('SELECT "ID","台語羅馬字","台語羅馬字2","台語漢字","華語對譯","英文","pos_h" ' + 
-		'FROM "楊允言先生資料"."台華" WHERE "ID"= 658 ORDER BY "ID"')
+		'FROM "楊允言先生資料"."台華" WHERE "ID"<= 658 ORDER BY "ID"')
 	辭典名 = '楊允言台華辭典'
 	
 	初胚工具 = 文章初胚工具()
 	分析器 = 拆文分析器()
 	轉音家私 = 轉物件音家私()
 	譀鏡 = 物件譀鏡()
+	
+	轉做咱講=轉做咱講工具()
 	def __init__(self):
-		print(self.檔頭())
+		print(self.轉做咱講.檔頭())
 		for ID, 台語羅馬字, 台語羅馬字2, 台語漢字, 華語對譯, 英文, pos_h in self.揣台華資料():
 			try:
 # 				print(ID, 台語羅馬字, 台語羅馬字2, 台語漢字, 華語對譯, 英文, pos_h)
@@ -71,109 +66,28 @@ class 整合台華:
 				pass
 			else:
 				詞性 = pos_h.strip()
-				條目頭=self.條目頭(臺語資料[0])
+				條目頭=self.轉做咱講.條目頭(臺語資料[0])
 				print(條目頭)
 				if 詞性 == '':
-					資料=self.一个意思(None,
+					資料=self.轉做咱講.一个意思(None,
 						'、'.join(國語資料)+'。',','.join(英文資料)+'.')
 					print(資料)
 				else:
 					for 孤單詞性 in 詞性:
 						if 孤單詞性 != '':
-							資料=self.一个意思(孤單詞性,
+							資料=self.轉做咱講.一个意思(孤單詞性,
 								'、'.join(國語資料)+'。',','.join(英文資料)+'.')
 							print(資料)
-				條目尾=self.條目尾()
+				條目尾=self.轉做咱講.條目尾()
 				print(條目尾)
 				for 後壁資料 in 臺語資料[1:]:
-					條目頭=self.條目頭(後壁資料)
+					條目頭=self.轉做咱講.條目頭(後壁資料)
 					print(條目頭)
-					參考=self.參考別人(臺語資料[0])
+					參考=self.轉做咱講.參考別人(臺語資料[0])
 					print(參考)
-					條目尾=self.條目尾()
+					條目尾=self.轉做咱講.條目尾()
 					print(條目尾)
-		print(self.檔尾())
-					
+		print(self.轉做咱講.檔尾())
 				
-	def 檔頭(self):
-		return self.檔頭表
-	def 檔尾(self):
-		return self.檔尾表
-	def 條目雜湊資料(self,臺語字音):
-		雜湊=hashlib.md5()
-		雜湊.update((臺語字音[0]+臺語字音[1]).encode(encoding='utf_8', errors='strict'))
-		return 雜湊.hexdigest()
-	def 條目頭(self,臺語字音):
-		臺語字,臺語音=臺語字音
-		雜湊=self.條目雜湊資料(臺語字音)
-		return self.條目頭表.format(臺語字,臺語音,雜湊)
-	def 一个意思(self,詞性,中文,英文):
-		詞=''
-		中=''
-		英=''
-		if 詞性!=None:
-			詞=self.詞性表.format(詞性)
-		if 中文!=None:
-			中=self.中文表.format(中文)
-		if 英文!=None:
-			英=self.英文表.format(英文)
-		意思=self.意思表.format(詞,中,英)
-		雜湊=hashlib.md5()
-		雜湊.update(意思.encode(encoding='utf_8', errors='strict'))
-		雜湊資料=雜湊.hexdigest()
-		return 意思.format(雜湊資料)
-	def 參考別人(self,第一人):
-		臺語字,臺語音=第一人
-		雜湊=self.條目雜湊資料(第一人)
-		return self.參考表.format(臺語字,臺語音,雜湊)
-		
-	檔頭表=(
-"""<?xml version="1.0" encoding="utf-8"?>
-<lift
-    version="0.13"
-    producer="Palaso.DictionaryServices.LiftWriter 2.1.27.0">""")
-	檔尾表="</lift>" 
-	參考表=(
-"""	<relation
-		type="confer"
-		ref="{0} {1}_{2}" />""")
-	def 條目尾(self):
-		return self.條目尾表
-	條目頭表=(	
-"""	<entry
-		id="{0} {1}_{2}"
-		dateCreated="2013-12-16T13:53:11Z"
-		dateModified="2013-12-16T13:56:48Z"
-		guid="{2}">
-		<lexical-unit>
-			<form
-				lang="nan">
-				<text>{0} {1}</text>
-			</form>
-		</lexical-unit>""")
-	條目尾表="	</entry>"
-	意思表=(
-"""	<sense
-		id="{{0}}">
-		<definition>
-{0}
-{1}
-{2}
-		</definition>
-	</sense>""")
-	詞性表=(
-"""		<grammatical-info
-			value="{0}" />""")
-	中文表=(
-"""	<form
-		lang="cmn">
-		<text>{0}</text>
-	</form>""")
-	英文表=(
-"""	<form
-		lang="en">
-		<text>{0}</text>
-	</form>""")
-		
 if __name__ == '__main__':
 	整合台華()
