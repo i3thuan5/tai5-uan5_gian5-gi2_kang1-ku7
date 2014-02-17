7283a1afdd3d970d8acbf8b9d69ca8646cbf41ef
Ihc <Ihc@gmail.com>
2014-02-17 20:43:19 +0800

    譀鏡的錯誤先抽出來，賰的沓沓仔來　丞宏

--- 測試/單一功能/字詞組集句章/解析整理工具/程式掠漏測試.py
+++ 測試/單一功能/字詞組集句章/解析整理工具/程式掠漏測試.py
@@ -18,17 +18,26 @@
 import unittest
 from 字詞組集句章.解析整理工具.解析錯誤 import 解析錯誤
 from 字詞組集句章.解析整理工具.型態錯誤 import 型態錯誤
+from 字詞組集句章.解析整理工具.拆文分析器 import 拆文分析器
+from 字詞組集句章.解析整理工具.程式掠漏 import 程式掠漏
 
 class 程式掠漏測試(unittest.TestCase):
 	def setUp(self):
 		self.掠漏 = 程式掠漏()
+		self.分析器 = 拆文分析器()
 	def tearDown(self):
 		pass
 
 	def test_毋是物件(self):
-		self.assertRaises(型態錯誤, self.掠漏.毋是字物件())
-		self.assertRaises(型態錯誤, self.掠漏.毋是詞物件())
-		self.assertRaises(型態錯誤, self.掠漏.毋是組物件())
-		self.assertRaises(型態錯誤, self.掠漏.毋是集物件())
-		self.assertRaises(型態錯誤, self.掠漏.毋是句物件())
-		self.assertRaises(型態錯誤, self.掠漏.毋是章物件())
+		self.掠漏.毋是字物件就毋著(self.分析器.建立字物件('語句'))
+		self.掠漏.毋是詞物件就毋著(self.分析器.建立詞物件('語句'))
+		self.掠漏.毋是組物件就毋著(self.分析器.建立組物件('語句'))
+		self.掠漏.毋是集物件就毋著(self.分析器.建立集物件('語句'))
+		self.掠漏.毋是句物件就毋著(self.分析器.建立句物件('語句'))
+		self.掠漏.毋是章物件就毋著(self.分析器.建立章物件('語句'))
+		self.assertRaises(型態錯誤, self.掠漏.毋是字物件就毋著, None)
+		self.assertRaises(型態錯誤, self.掠漏.毋是詞物件就毋著, 2)
+		self.assertRaises(型態錯誤, self.掠漏.毋是組物件就毋著, 'sui2')
+		self.assertRaises(型態錯誤, self.掠漏.毋是集物件就毋著, '我')
+		self.assertRaises(型態錯誤, self.掠漏.毋是句物件就毋著, self.分析器.建立章物件('語句'))
+		self.assertRaises(型態錯誤, self.掠漏.毋是章物件就毋著, self.分析器.建立句物件('語句'))
