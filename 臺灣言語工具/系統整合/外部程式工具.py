d48216464c985a6fa3e4a9926ea7729ea80d9da6
Ihc <Ihc@gmail.com>
2014-03-15 17:34:13 +0800

    改揣目錄的方法 丞宏

--- 臺灣言語工具/系統整合/外部程式工具.py
+++ 臺灣言語工具/系統整合/外部程式工具.py
@@ -15,14 +15,11 @@
 
 感謝您的使用與推廣～～勞力！承蒙！
 """
-from subprocess import Popen, PIPE
+import os
 
 class 外部程式工具:
 	def 專案目錄(self):
-		找位置 = Popen(['pwd'], stdout = PIPE)
-		for 一逝 in 找位置.stdout:
-			程式所在 = 一逝[:-1].decode("utf-8")
-		找位置.stdout.close()
+		程式所在 = os.path.abspath(__file__)
 		程式所在, 資料夾 = 程式所在.rsplit('/', 1)
 		while 資料夾 != '臺灣言語工具':
 			程式所在, 資料夾 = 程式所在.rsplit('/', 1)
