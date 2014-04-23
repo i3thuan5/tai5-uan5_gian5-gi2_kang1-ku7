40e492bba6ef38a341d2b4e2b6121ee06d22e427
Ihc <ihcaoe@gmail.com>
2014-04-23 16:16:29 +0800

    編碼用指令有的 丞宏

--- 臺灣言語工具/翻譯/摩西工具/語句編碼器.py
+++ 臺灣言語工具/翻譯/摩西工具/語句編碼器.py
@@ -4,9 +4,11 @@ from urllib.parse import unquote
 
 class 語句編碼器:
 	def 編碼(self,語句):
-		return quote(語句,safe=' \n')
+		'''會當配合jdk的native2ascii'''
+		return 語句.encode('unicode_escape').decode('ascii')
 	def 解碼(self,語句):
-		return unquote(語句)
+		'''會當配合jdk的native2ascii -reverse'''
+		return 語句.encode('ascii').decode('unicode_escape')
 
 if __name__=='__main__':
 	來='/home/Ihc/mt/編碼/翻.臺語斷詞.txt'
