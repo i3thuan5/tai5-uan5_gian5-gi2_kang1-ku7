771f8988c8f2c7a41f07e8c39fa37c5aa275767f
SIDK <Ihc@gmail.com>
2014-04-23 17:19:51 +0800

    加網仔試驗 丞宏

--- 臺灣言語工具/試驗/單一功能/字詞組集句章/解析整理/詞物件網仔試驗.py
+++ 臺灣言語工具/試驗/單一功能/字詞組集句章/解析整理/詞物件網仔試驗.py
@@ -19,6 +19,7 @@
 import unittest
 from 臺灣言語工具.字詞組集句章.解析整理.拆文分析器 import 拆文分析器
 from 臺灣言語工具.字詞組集句章.基本元素.集 import 集
+from 臺灣言語工具.字詞組集句章.基本元素.章 import 章
 from 臺灣言語工具.字詞組集句章.解析整理.解析錯誤 import 解析錯誤
 from 臺灣言語工具.字詞組集句章.解析整理.型態錯誤 import 型態錯誤
 from 臺灣言語工具.字詞組集句章.解析整理.文章粗胚 import 文章粗胚
@@ -38,128 +39,90 @@ class 詞物件網仔試驗(unittest.TestCase):
 		詞物件 = self.分析器.建立詞物件(型)
 		self.assertEqual(self.網仔.網出詞物件(字物件), [詞物件])
 		self.assertEqual(self.網仔.網出詞物件(詞物件), [詞物件])
-#＃＃＃＃＃＃＃
-	def test_網組孤字(self):
-		raise NotImplementedError
-		型 = '媠'
-		字物件 = self.分析器.建立字物件(型)
+
+	def test_網詞無字(self):
+		型 = ''
 		詞物件 = self.分析器.建立詞物件(型)
-		self.assertEqual(self.網仔.網出字物件(詞物件), [字物件])
-# 
-# 	def test_網詞無字(self):
-# 		型 = ''
-# 		詞物件 = self.分析器.建立詞物件(型)
-# 		self.assertEqual(self.網仔.網出字物件(詞物件), [])
-# 
-# 	def test_網詞濟字漢字(self):
-# 		語句 = '椅仔！'
-# 		self.assertEqual(
-# 			self.網仔.網出字物件(self.分析器.建立詞物件(語句)),
-# 			[self.分析器.建立字物件('椅'), self.分析器.建立字物件('仔'), self.分析器.建立字物件('！')])
-# 
-# 	def test_網詞濟字音標(self):
-# 		語句 = 'tsit8-tiunn1 !'
-# 		self.assertEqual(
-# 			self.網仔.網出字物件(self.分析器.建立詞物件(語句)),
-# 			[self.分析器.建立字物件('tsit8'), self.分析器.建立字物件('tiunn1'), self.分析器.建立字物件('!')])
-# 
-# 	def test_網詞濟字漢羅(self):
-# 		語句 = 'tsit8-張!'
-# 		self.assertEqual(
-# 			self.網仔.網出字物件(self.分析器.建立詞物件(語句)),
-# 			[self.分析器.建立字物件('tsit8'), self.分析器.建立字物件('張'), self.分析器.建立字物件('!')])
-# 
-# 	def test_網組孤字(self):
-# 		型 = '媠'
-# 		字物件 = self.分析器.建立字物件(型)
-# 		組物件 = self.分析器.建立組物件(型)
-# 		self.assertEqual(self.網仔.網出字物件(組物件), [字物件])
-# 
-# 	def test_網組無字(self):
-# 		型 = ''
-# 		組物件 = self.分析器.建立組物件(型)
-# 		self.assertEqual(self.網仔.網出字物件(組物件), [])
-# 
-# 	def 建立組檢查(self, 原來語句, 切好語句):
-# 		字陣列 = []
-# 		[字陣列.extend(self.網仔.網出字物件(self.分析器.建立詞物件(詞)))
-# 			for 詞 in 切好語句]
-# 		return (self.分析器.建立組物件(原來語句), 字陣列)
-# 
-# 	def test_網組濟字(self):
-# 		原來語句 = '我有一張椅仔！'
-# 		切好語句 = ['我', '有', '一', '張', '椅', '仔', '！']
-# 		組物件, 詞陣列 = self.建立組檢查(原來語句, 切好語句)
-# 		self.assertEqual(self.網仔.網出字物件(組物件), 詞陣列)
-# 
-# 	# 為著通用佮一致性，這愛家己建立詞來鬥。大部份攏是無細字揤著，親像平行語料庫才另外閣包一層
-# 	def test_網組濟字配空白(self):
-# 		原來語句 = '我 有 一張 椅仔！'
-# 		切好語句 = ['我', '有', '一', '張', '椅', '仔', '！']
-# 		組物件, 詞陣列 = self.建立組檢查(原來語句, 切好語句)
-# 		self.assertEqual(self.網仔.網出字物件(組物件), 詞陣列)
-# 
-# 	def test_網組濟音標(self):
-# 		原來語句 = 'gua2 u7 tsit8-tiunn1 i2-a2'
-# 		處理好語句 = 'gua2 u7 tsit8-tiunn1 i2-a2'
-# 		加空白後語句 = 'gua2 u7 tsit8-tiunn1 i2-a2'
-# 		self.assertEqual(self.粗胚.建立物件語句前處理減號(臺灣閩南語羅馬字拼音, 原來語句), 處理好語句)
-# 		self.assertEqual(self.粗胚.符號邊仔加空白(處理好語句), 加空白後語句)
-# 		切好語句 = ['gua2', 'u7', 'tsit8-tiunn1', 'i2-a2']
-# 		組物件, 詞陣列 = self.建立組檢查(處理好語句, 切好語句)
-# 		self.assertEqual(self.網仔.網出字物件(組物件), 詞陣列)
-# 
-# 	def test_網組濟字輕聲(self):
-# 		原來語句 = 'mi2-kiann7 boo5-0ki3 ah!'
-# 		處理好語句 = 'mi2-kiann7 boo5-0ki3 ah!'
-# 		加空白後語句 = 'mi2-kiann7 boo5-0ki3 ah ! '
-# 		self.assertEqual(self.粗胚.建立物件語句前處理減號(臺灣閩南語羅馬字拼音, 原來語句), 處理好語句)
-# 		self.assertEqual(self.粗胚.符號邊仔加空白(處理好語句), 加空白後語句)
-# 		切好語句 = ['mi2-kiann7', 'boo5-0ki3', 'ah', '!']
-# 		組物件, 詞陣列 = self.建立組檢查(處理好語句, 切好語句)
-# 		self.assertEqual(self.網仔.網出字物件(組物件), 詞陣列)
-# 
-# 	def test_網句濟字佮符號(self):
-# 		原來語句 = '枋寮漁港「大條巷」上闊兩公尺。'
-# 		切好語句 = ['枋', '寮', '漁', '港', '「', '大', '條', '巷', '」', '上', '闊', '兩', '公', '尺', '。']
-# 		組物件, 詞陣列 = self.建立組檢查(原來語句, 切好語句)
-# 		self.assertEqual(self.網仔.網出字物件(
-# 			self.分析器.建立句物件(原來語句)), 詞陣列)
-# 		組物件 = 組物件
-# 
-# 	def test_網章濟連字佮符號(self):
-# 		原來語句 = '枋-寮漁-港「大-條-巷」。上-闊兩-公-尺'
-# 		切好語句 = ['枋寮', '漁港', '「', '大條巷', '」', '。', '上闊', '兩公尺']
-# 		組物件, 詞陣列 = self.建立組檢查(原來語句, 切好語句)
-# 		self.assertEqual(self.網仔.網出字物件(
-# 			self.分析器.建立章物件(原來語句)), 詞陣列)
-# 		組物件 = 組物件
-# 
-# 	def test_網集濟組(self):
-# 		原來語句 = '我有一張椅仔！'
-# 		組陣列 = [self.分析器.建立組物件(原來語句),
-# 			self.分析器.建立組物件(原來語句), ]
-# 		self.assertRaises(解析錯誤, self.網仔.網出字物件, 集(組陣列))
-# 
-# 	def test_網集無字(self):
-# 		型 = ''
-# 		集物件 = self.分析器.建立集物件(型)
-# 		self.assertEqual(self.網仔.網出字物件(集物件), [])
-# 
-# 	def test_網句無字(self):
-# 		型 = ''
-# 		句物件 = self.分析器.建立句物件(型)
-# 		self.assertEqual(self.網仔.網出字物件(句物件), [])
-# 
-# 	def test_網章無字(self):
-# 		型 = ''
-# 		章物件 = self.分析器.建立章物件(型)
-# 		self.assertEqual(self.網仔.網出字物件(章物件), [])
-# 
-# 	def test_烏白擲物件(self):
-# 		self.assertRaises(型態錯誤, self.網仔.網出字物件, 2123)
-# 		self.assertRaises(型態錯誤, self.網仔.網出字物件, self.網仔)
-# 		self.assertRaises(型態錯誤, self.網仔.網出字物件, None)
-# 
-# if __name__ == '__main__':
-# 	unittest.main()
+		self.assertEqual(self.網仔.網出詞物件(詞物件), [詞物件])
+ 
+	def test_網詞濟字漢字(self):
+		語句 = '椅仔！'
+		self.assertEqual(
+			self.網仔.網出詞物件(self.分析器.建立詞物件(語句)),
+			[self.分析器.建立詞物件(語句)])
+ 
+	def test_網詞濟字音標(self):
+		語句 = 'tsit8-tiunn1 !'
+		self.assertEqual(
+			self.網仔.網出詞物件(self.分析器.建立詞物件(語句)),
+			[self.分析器.建立詞物件(語句)])
+ 
+	def test_網詞濟字漢羅(self):
+		語句 = 'tsit8-張!'
+		self.assertEqual(
+			self.網仔.網出詞物件(self.分析器.建立詞物件(語句)),
+			[self.分析器.建立詞物件(語句)])
+ 
+	def test_網組孤字(self):
+		語句 = '媠'
+		切好語句 = ['媠']
+		self.網組集句章看覓(語句, 切好語句)
+ 
+	def test_網無字(self):
+		語句 = ''
+		切好語句 = []
+		self.網組集句章看覓(語句, 切好語句)
+ 
+	def test_網濟音標(self):
+		語句 = 'gua2 u7 tsit8-tiunn1 i2-a2'
+		切好語句 = ['gua2', 'u7', 'tsit8-tiunn1', 'i2-a2']
+		self.網組集句章看覓(語句, 切好語句)
+		
+	def test_網濟漢羅(self):
+		語句 = 'gua2 有 tsit8-tiunn1 椅-仔'
+		切好語句 = ['gua2', '有', 'tsit8-tiunn1', '椅仔']
+		self.網組集句章看覓(語句, 切好語句)
+ 
+	def test_網濟字輕聲(self):
+		語句 = 'mi2-kiann7 boo5-0ki3 ah ! '
+		切好語句 = ['mi2-kiann7', 'boo5-0ki3', 'ah', '!']
+		self.網組集句章看覓(語句, 切好語句)
+  
+	def test_網濟連字佮符號(self):
+		語句 = '枋-寮漁-港「大-條-巷」。上-闊兩-公-尺'
+		切好語句 = ['枋寮', '漁港', '「', '大條巷', '」', '。', '上闊', '兩公尺']
+		self.網組集句章看覓(語句, 切好語句)
+		
+	def 網組集句章看覓(self, 語句, 切好語句):
+		切好詞陣列 = []
+		for 語詞 in 切好語句:
+			切好詞陣列.append(self.分析器.建立詞物件(語詞))
+		self.assertEqual(
+			self.網仔.網出詞物件(self.分析器.建立組物件(語句)),
+			切好詞陣列)
+		self.assertEqual(
+			self.網仔.網出詞物件(self.分析器.建立集物件(語句)),
+			切好詞陣列)
+		self.assertEqual(
+			self.網仔.網出詞物件(self.分析器.建立句物件(語句)),
+			切好詞陣列)
+		self.assertEqual(
+			self.網仔.網出詞物件(self.分析器.建立章物件(語句)),
+			切好詞陣列)
+ 
+	def test_網集濟組(self):
+		原來語句 = '我有一張椅仔！'
+		組陣列 = [self.分析器.建立組物件(原來語句),
+			self.分析器.建立組物件(原來語句), ]
+		self.assertRaises(解析錯誤, self.網仔.網出詞物件, 集(組陣列))
+		
+	def test_網章濟句(self):
+		原來語句 = '我有一張椅仔！'
+		句陣列 = [self.分析器.建立句物件(原來語句)] * 2
+		self.assertEqual(self.網仔.網出詞物件(章(句陣列)),
+			self.網仔.網出詞物件(self.分析器.建立句物件(原來語句)) * 2)
+ 
+	def test_烏白擲物件(self):
+		self.assertRaises(型態錯誤, self.網仔.網出詞物件, 2123)
+		self.assertRaises(型態錯誤, self.網仔.網出詞物件, self.網仔)
+		self.assertRaises(型態錯誤, self.網仔.網出詞物件, None)
