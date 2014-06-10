cbbb69ab58b57af59e6ecdf93d9bcf11798d557e
Ihc <Ihc@gmail.com>
2014-06-10 22:31:04 +0800

    先試基本的 丞宏

--- 臺灣言語工具/試驗/斷詞/辭典連詞斷詞試驗.py
+++ 臺灣言語工具/試驗/斷詞/辭典連詞斷詞試驗.py
@@ -28,14 +28,95 @@ from 臺灣言語工具.基本元素.章 import 章
 from 臺灣言語工具.解析整理.解析錯誤 import 解析錯誤
 from 臺灣言語工具.表單.語句連詞 import 語句連詞
 from 臺灣言語工具.斷詞.連詞揀集內組 import 連詞揀集內組
+from 臺灣言語工具.斷詞.辭典連詞斷詞 import 辭典連詞斷詞
 
 class 辭典連詞斷詞試驗(TestCase):
 	忍受 = 1e-10
 	def setUp(self):
-		pass
+		self.斷詞 = 辭典連詞斷詞()
+		self.字典 = 型音辭典(4)
+		self.連詞 = 語句連詞(3)
+
+		self.分析器 = 拆文分析器()
+
+		self.我對齊詞 = self.分析器.產生對齊詞('我', 'gua2')
+		self.文我對齊詞 = self.分析器.產生對齊詞('我', 'ngoo2')
+		self.有對齊詞 = self.分析器.產生對齊詞('有', 'u7')
+		self.一張對齊詞 = self.分析器.產生對齊詞('一張', 'tsit8-tiunn1')
+		self.椅仔對齊詞 = self.分析器.產生對齊詞('椅仔', 'i2-a2')
+		self.驚對齊詞 = self.分析器.產生對齊詞('！', '!')
+
+		self.對齊句 = self.分析器.產生對齊句(
+			'我有一張椅仔！！', 'gua2 u7 tsit8-tiunn1 i2-a2!!')
+		self.型句 = self.分析器.建立句物件('我有一張椅仔！！')
+		self.音句 = self.分析器.建立句物件('gua2 u7 tsit8-tiunn1 i2-a2!!')
+		self.有詞漢羅 = self.分析器.建立句物件('我 u7 一張 i2-a2!!')
+		self.無詞漢羅 = self.分析器.建立句物件('gua2 u7 一張 i2-a2!!')
+
+		self.予對齊詞 = self.分析器.產生對齊詞('予', 'hoo7')
+		self.伊對齊詞 = self.分析器.產生對齊詞('伊', 'i1')
+		self.雨衣對齊詞 = self.分析器.產生對齊詞('雨衣', 'hoo7-i1')
+		self.出去對齊詞 = self.分析器.產生對齊詞('出去', 'tsut4-khi2')
+		self.耍對齊詞 = self.分析器.產生對齊詞('耍', 'sng2')
+
+		self.予伊出去耍全羅 = self.分析器.建立句物件('hoo7 i1 tsut4 khi2 sng2')
+		self.予伊對齊句 = self.分析器.產生對齊句(
+			'予伊出去耍', 'hoo7 i1 tsut4-khi3 sng2')
+		self.雨衣對齊句 = self.分析器.產生對齊句(
+			'雨衣出去耍', 'hoo7-i1 tsut4-khi3 sng2')
+		self.予伊耍對齊句 = self.分析器.產生對齊句(
+			'予伊耍雨衣', 'hoo7 i1 sng2 hoo7-i1')
+
+
 
 	def tearDown(self):
 		pass
+
+	def test_句無連詞(self):
+		self.字典.加詞(self.我對齊詞)
+		self.字典.加詞(self.有對齊詞)
+		self.字典.加詞(self.一張對齊詞)
+		self.字典.加詞(self.椅仔對齊詞)
+		self.字典.加詞(self.驚對齊詞)
+		斷詞結果, 分數, 詞數 = self.斷詞.斷詞(self.字典, self.連詞, self.對齊句)
+		self.assertEqual(斷詞結果, self.句物件)
+		self.檢查分數詞數(分數, 詞數, 0, 6)
+		斷詞結果, 分數, 詞數 = self.斷詞.斷詞(self.字典, self.連詞, self.型句)
+		self.assertEqual(斷詞結果, self.句物件)
+		self.檢查分數詞數(分數, 詞數, 0, 6)
+		斷詞結果, 分數, 詞數 = self.斷詞.斷詞(self.字典, self.連詞, self.音句)
+		self.assertEqual(斷詞結果, self.句物件)
+		self.檢查分數詞數(分數, 詞數, 0, 6)
+		斷詞結果, 分數, 詞數 = self.斷詞.斷詞(self.字典, self.連詞, self.有詞漢羅)
+		self.assertEqual(斷詞結果, self.句物件)
+		self.檢查分數詞數(分數, 詞數, 0, 6)
+		斷詞結果, 分數, 詞數 = self.斷詞.斷詞(self.字典, self.連詞, self.無詞漢羅)
+		self.assertEqual(斷詞結果, self.句物件)
+		self.檢查分數詞數(分數, 詞數, 0, 6)
+
+	def test_句毋是愈長愈好(self):
+		self.予對齊詞 = self.分析器.產生對齊詞('予', 'hoo7')
+		self.伊對齊詞 = self.分析器.產生對齊詞('伊', 'i1')
+		self.雨衣對齊詞 = self.分析器.產生對齊詞('雨衣', 'hoo7-i1')
+		self.出去對齊詞 = self.分析器.產生對齊詞('出去', 'tsut4-khi2')
+		self.耍對齊詞 = self.分析器.產生對齊詞('耍', 'sng2')
+		self.字典.加詞(self.予對齊詞)
+		self.字典.加詞(self.伊對齊詞)
+		self.字典.加詞(self.出去對齊詞)
+		self.字典.加詞(self.耍對齊詞)
+		斷詞結果, 分數, 詞數 = self.斷詞.斷詞(self.字典, self.連詞, self.對齊句)
+		self.assertEqual(斷詞結果, self.予伊對齊句)
+		self.檢查分數詞數(分數, 詞數, 0, 4)
+
+		self.字典.加詞(self.雨衣對齊詞)
+		斷詞結果, 分數, 詞數 = self.斷詞.斷詞(self.字典, self.連詞, self.型句)
+		self.assertEqual(斷詞結果, self.雨衣對齊句)
+		self.檢查分數詞數(分數, 詞數, 0, 3)
+
+		self.連詞.看(self.self.予伊耍對齊句)
+		斷詞結果, 分數, 詞數 = self.斷詞.斷詞(self.字典, self.連詞, self.音句)
+		self.assertEqual(斷詞結果, self.句物件)
+		self.檢查分數詞數(分數, 詞數, 0, 4)
 
 	def test_試驗好矣未(self):
 		raise NotImplementedError
