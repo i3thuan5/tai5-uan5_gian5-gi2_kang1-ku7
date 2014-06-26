5b6c95471e7c8ffe8b887f7ac2bf8d0292608832
chhsueh <ihcaod@gmail.com>
2014-06-26 14:30:31 +0800

    標仔處理好矣 丞宏

--- 語音辨識/辨識模型.py
+++ 語音辨識/辨識模型.py
@@ -1,4 +1,5 @@
 import os
+import wave
 
 class 辨識模型:
 	wav副檔名 = '.wav'
@@ -6,25 +7,34 @@ class 辨識模型:
 	標仔副檔名 = '.lab'
 	特徵 = 'mfcc'
 	特徵副檔名 = '.' + 特徵
-	def 訓練(self, 音檔目錄, 標仔目錄, 資料目錄, 執行檔路徑=''):
+	def 訓練(self, 音檔目錄, 標仔目錄, 資料目錄, 執行檔路徑='',
+			算特徵=True, 切標仔=True):
 		if 執行檔路徑 != '' and not 執行檔路徑.endswith('/'):
 			執行檔路徑 = 執行檔路徑 + '/'
 		全部語料 = self.揣全部語料(音檔目錄, 標仔目錄)
+		
+# 		wave.open(全部語料[0][1])
 		全部特徵檔 = os.path.join(資料目錄, '全部特徵檔')
-		算特徵 = True
+		全部標仔檔 = os.path.join(資料目錄, '全部標仔檔')
 		if 算特徵:
 			參數檔 = '../config/mfcc39.cfg.44100'
+			self.字串寫入檔案(參數檔, self.算特徵參數)
 			特徵目錄 = os.path.join(資料目錄, self.特徵)
 			os.makedirs(特徵目錄, exist_ok=True)
 			全部特徵 = []
+			全部標仔 = []
 			for 語料名, 音檔所在, 標仔所在 in 全部語料:
 				特徵所在 = os.path.join(特徵目錄,
 					語料名 + self.特徵副檔名)
 				self.算特徵(執行檔路徑, 參數檔, 音檔所在, 特徵所在)
 				全部特徵.append(特徵所在)
+				全部標仔.append(標仔所在)
 			self.陣列寫入檔案(全部特徵檔, 全部特徵)
-	def 揣全部語料(self,音檔目錄,標仔目錄):
-		全部語料=[]
+			self.陣列寫入檔案(全部標仔檔, 全部標仔)
+		if 切標仔:
+			self.標仔加恬佮切開(執行檔路徑, 全部標仔檔, 資料目錄)
+	def 揣全部語料(self, 音檔目錄, 標仔目錄):
+		全部語料 = []
 		for 音檔檔名 in os.listdir(音檔目錄):
 			if 音檔檔名.endswith(self.音檔副檔名):
 				語料名 = 音檔檔名[:-len(self.音檔副檔名)]
@@ -39,14 +49,52 @@ class 辨識模型:
 			執行檔路徑, 參數檔, 音檔所在, 特徵所在)
 		print(特徵指令)
 		os.system(特徵指令)
+	def 標仔加恬佮切開(self, 執行檔路徑, 全部標仔檔, 資料目錄):
+		原來音節檔 = os.path.join(資料目錄, '原來音節檔')
+		原來音類檔 = os.path.join(資料目錄, '原來音類檔')
+		整理音節指令 = '{0}HLEd -l "*" -i {1} -n {2} -S {3} /dev/null'\
+			.format(執行檔路徑, 原來音節檔, 原來音類檔, 全部標仔檔)
+		os.system(整理音節指令)
+		加恬音節檔 = os.path.join(資料目錄, '加恬音節檔')
+		加恬音類檔 = os.path.join(資料目錄, '加恬音類檔')
+		加恬參數檔 = os.path.join(資料目錄, '加恬參數檔')
+		self.字串寫入檔案(加恬參數檔, 'IS sil sil')
+		加恬音節指令 = '{0}HLEd -l "*" -i {1} -n {2} {3} {4}'\
+			.format(執行檔路徑, 加恬音節檔, 加恬音類檔, 加恬參數檔, 原來音節檔)
+		os.system(加恬音節指令)
+
+		聲韻檔 = os.path.join(資料目錄, '聲韻檔')
+		聲韻類檔 = os.path.join(資料目錄, '聲韻類檔')
+		音節聲韻對照檔 = '../config/Syl2Monophone.dic.ok'  # os.path.join(資料目錄, '')
+		切聲韻參數檔 = os.path.join(資料目錄, '拆聲韻參數檔')
+		self.字串寫入檔案(切聲韻參數檔, 'EX')
+		切聲韻指令 = '{0}HLEd -l "*" -i {1} -n {2} -d {3} {4} {5}'\
+			.format(執行檔路徑, 聲韻檔, 聲韻類檔, 音節聲韻對照檔, 切聲韻參數檔, 加恬音節檔)
+		os.system(切聲韻指令)
 	def 陣列寫入檔案(self, 檔名, 陣列):
 		self.字串寫入檔案(檔名, '\n'.join(陣列))
 	def 字串寫入檔案(self, 檔名, 字串):
 		檔案 = open(檔名, 'w')
 		print(字串, file=檔案)
 		檔案.close()
+	算特徵參數 = \
+'''
+SOURCEKIND = WAVEFORM
+SOURCEFORMAT = WAV
+TARGETKIND = MFCC_E_D_A_Z
+TARGETRATE = 100000.0
+WINDOWSIZE = 200000.0
+PREEMCOEF = 0.975
+NUMCHANS = 26
+CEPLIFTER = 22
+NUMCEPS = 12
+USEHAMMING = T
+DELTAWINDOW = 2	
+ACCWINDOW= 2
+'''
 if __name__ == '__main__':
 	模型 = 辨識模型()
 	這馬目錄 = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-	模型.訓練(這馬目錄 + '/wav', 這馬目錄 + '/labels', 這馬目錄 + '/data')
+	模型.訓練(這馬目錄 + '/wav', 這馬目錄 + '/labels', 這馬目錄 + '/data',
+		算特徵=False)
 		
