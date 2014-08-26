a246007a7f4f84ff4df4a7200d821af9b9728384
sih4sing5hong5 <ihcaoe@gmail.com>
2014-08-26 09:47:45 +0800

    共綜合標音預設莫因為拼音問題出現例外，這是資料問題 毋過型態問題嘛是照例外，因為這是設計的問題 fix #36 丞宏

--- 試驗/綜合標音/官話字綜合標音試驗.py
+++ 試驗/綜合標音/官話字綜合標音試驗.py
@@ -35,15 +35,22 @@ class 官話字綜合標音試驗(unittest.TestCase):
 	def test_合法(self):
 		綜合標音 = 官話字綜合標音(字(self.我型, self.我音))
 		self.assertEqual(綜合標音.標音完整無(), True)
+		綜合標音 = 官話字綜合標音(字(self.我型, self.我音), False)
+		self.assertEqual(綜合標音.標音完整無(), True)
+		綜合標音 = 官話字綜合標音(字(self.我型, self.我音), True)
+		self.assertEqual(綜合標音.標音完整無(), True)
 	def test_兩个字合法(self):
 		我綜合標音 = 官話字綜合標音(字(self.我型, self.我音))
 		你綜合標音 = 官話字綜合標音(字(self.你型, self.你音))
+		self.assertEqual(我綜合標音.標音完整無(), True)
+		self.assertEqual(你綜合標音.標音完整無(), True)
 		self.assertEqual(我綜合標音.型體, self.我型)
 		self.assertEqual(你綜合標音.型體, self.你型)
 	def test_轉json格式(self):
 		綜合標音 = 官話字綜合標音(字(self.我型, self.我音))
 		self.assertEqual(綜合標音.轉json格式(),
 			{"型體":self.我型, "注音符號":self.我標音})
+		self.assertEqual(綜合標音.標音完整無(), True)
 	def test_標點合法(self):
 		標點 = 官話字綜合標音(字('，', 無音))
 		self.assertEqual(標點.標音完整無(), True)
@@ -51,13 +58,20 @@ class 官話字綜合標音試驗(unittest.TestCase):
 		標點 = 官話字綜合標音(字('，', 無音))
 		self.assertEqual(標點.轉json格式(), {"型體":"，", "注音符號":""})
 	def test_標點音無合法(self):
-		self.assertRaises(解析錯誤, 官話字綜合標音, 字('我', 'ㄆㄨㄧˋ'))
+		標音 = 官話字綜合標音(字('我', 'ㄆㄨㄧˋ'))
+		self.assertEqual(標音.標音完整無(), False)
+		標音 = 官話字綜合標音(字('我', 'ㄆㄨㄧˋ'), False)
+		self.assertEqual(標音.標音完整無(), False)
+		self.assertRaises(解析錯誤, 官話字綜合標音, 字('我', 'ㄆㄨㄧˋ'), True)
 	def test_烏白傳(self):
 		self.assertRaises(型態錯誤, 官話字綜合標音, '我')
+		self.assertRaises(型態錯誤, 官話字綜合標音, '我', False)
+		self.assertRaises(型態錯誤, 官話字綜合標音, '我', True)
 	def test_輕聲轉json格式(self):
-		型='的'
-		音='ㄉㄜ˙'
+		型 = '的'
+		音 = 'ㄉㄜ˙'
 		綜合標音 = 官話字綜合標音(字(型, 音))
+		self.assertEqual(綜合標音.標音完整無(), True)
 		self.assertEqual(綜合標音.轉json格式(),
 			{"型體":型, "注音符號":'⿳⿳˙ㄉㄜ'})
 
