486ff390b10add152d55232f3529f71eb09321e9
薛丞宏 <ihcaoe@gmail.com>
2014-09-17 18:40:10 +0800

    改變數名 丞宏

--- 臺灣言語工具/表單/尾字辭典.py
+++ 臺灣言語工具/表單/尾字辭典.py
@@ -18,23 +18,23 @@
 """
 from 臺灣言語工具.基本元素.詞 import 詞
 
-def _詞物件倒過來(詞物件):
-	倒過來詞物件 = 詞()
-	倒過來詞物件.內底字 = 詞物件.內底字[::-1]
-	return 倒過來詞物件
+def _詞物件反過來(詞物件):
+	反過來詞物件 = 詞()
+	反過來詞物件.內底字 = 詞物件.內底字[::-1]
+	return 反過來詞物件
 
 def _加詞(self, 詞物件):
-	return super(self.__class__, self).加詞(_詞物件倒過來(詞物件))
+	return super(self.__class__, self).加詞(_詞物件反過來(詞物件))
 	
 def _查詞(self, 詞物件):
-	結果陣列 = super(self.__class__, self).查詞(_詞物件倒過來(詞物件))
-	倒過來結果陣列 = []
+	結果陣列 = super(self.__class__, self).查詞(_詞物件反過來(詞物件))
+	反過來結果陣列 = []
 	for 結果 in 結果陣列:
-		倒過來結果 = set()
+		反過來結果 = set()
 		for 一个詞 in 結果:
-			倒過來結果.add(_詞物件倒過來(一个詞))
-		倒過來結果陣列.append(倒過來結果)
-	return 倒過來結果陣列
+			反過來結果.add(_詞物件反過來(一个詞))
+		反過來結果陣列.append(反過來結果)
+	return 反過來結果陣列
 	
 def 尾字辭典(辭典類):
 	return type('尾字{0}'.format(辭典類.__class__),
