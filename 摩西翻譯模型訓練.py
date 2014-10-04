76a0b25e9526c3ddc81ad23268e5daf91fb801a8
薛丞宏 <ihcaoe@gmail.com>
2014-10-04 11:44:17 +0800

    加編碼器 丞宏

--- 摩西翻譯模型訓練.py
+++ 摩西翻譯模型訓練.py
@@ -19,33 +19,39 @@
 from 臺灣言語工具.語音辨識.腳本程式 import 腳本程式
 import gzip
 import os
+from 臺灣言語工具.翻譯.摩西工具.無編碼器 import 無編碼器
+import shutil
 
 class 摩西翻譯模型訓練(腳本程式):
 	def 訓練(self, 來源語言平行語料, 目標語言平行語料, 目標語言語料,
-			連紲詞長度,
 			暫存資料夾,
+			連紲詞長度=3,
+			編碼器=無編碼器(),
 			SRILM執行檔路徑='',
-			GIZA執行檔路徑='',#愛有 mkcls, GIZA++/mgiza, & snt2cooc.out/snt2cooc
+			GIZA執行檔路徑='',  # 愛有 mkcls, GIZA++/mgiza, & snt2cooc.out/snt2cooc
 			MOSES腳本路徑=''):
 		os.makedirs(暫存資料夾, exist_ok=True)
+		目標語言檔名 = os.path.join(暫存資料夾, '語言模型.txt')
+		self._檔案合做一个(目標語言檔名, 目標語言語料, 編碼器)
 		語言模型檔 = os.path.join(暫存資料夾, '語言模型.lm')
 		語言模型指令版 = \
-			'{0}ngram-count -order {1} -interpolate -wbdiscount -unk {2} -lm {3}'
+			'{0}ngram-count -order {1} -interpolate -wbdiscount -unk -text {2} -lm {3}'
 		語言模型指令 = 語言模型指令版.format(
 			self.執行檔路徑加尾(SRILM執行檔路徑),
 			連紲詞長度,
-			' '.join(['-text ' + 語料 for 語料 in 目標語言語料]),
+			目標語言檔名,
 			語言模型檔
 			)
 		self.走指令(語言模型指令)
 		
 		平行檔名 = os.path.join(暫存資料夾, '翻')
 		來源平行檔名 = os.path.join(暫存資料夾, '翻.源')
-		self._檔案合做一个(來源平行檔名, 來源語言平行語料)
+		self._檔案合做一个(來源平行檔名, 來源語言平行語料, 編碼器)
 		目標平行檔名 = os.path.join(暫存資料夾, '翻.目')
-		self._檔案合做一个(目標平行檔名, 目標語言平行語料)
+		self._檔案合做一个(目標平行檔名, 目標語言平行語料, 編碼器)
 		
 		翻譯模型資料夾 = os.path.join(暫存資料夾, '翻譯模型資料夾')
+		shutil.rmtree(翻譯模型資料夾)
 		翻譯模型指令版 = \
 			'SCRIPTS_ROOTDIR={1} {1}/training/train-model.perl -root-dir {2} -corpus {3} -f {4} -e {5} -alignment grow-diag-final-and -reordering msd-bidirectional-fe -lm 0:{7}:{6} -external-bin-dir={0}'
 		翻譯模型指令 = 翻譯模型指令版.format(
@@ -59,7 +65,7 @@ class 摩西翻譯模型訓練(腳本程式):
 			連紲詞長度,
 			)
 		self.走指令(翻譯模型指令)
-	def _檔案合做一个(self, 平行檔名, 語言平行語料):
+	def _檔案合做一个(self, 平行檔名, 語言平行語料, 編碼器):
 		with open(平行檔名, 'w') as 寫檔:
 			for 語言檔案 in 語言平行語料:
 				if 語言檔案.endswith('.gz'):
@@ -67,4 +73,5 @@ class 摩西翻譯模型訓練(腳本程式):
 				else:
 					開檔 = open
 				with 開檔(語言檔案, mode='rt') as 檔案:
-					print(檔案.read(), end='', file=寫檔)
+					for 一逝 in 檔案.readlines():
+						print(編碼器.編碼(一逝.strip()), file=寫檔)
--- 訓練摩西翻譯模型.py
+++ 訓練摩西翻譯模型.py
@@ -18,6 +18,7 @@
 """
 import os
 from 摩西翻譯模型訓練 import 摩西翻譯模型訓練
+from 臺灣言語工具.翻譯.摩西工具.語句編碼器 import 語句編碼器
 
 if __name__ == '__main__':
 	模型訓練 = 摩西翻譯模型訓練()
@@ -25,8 +26,9 @@ if __name__ == '__main__':
 		['/home/Ihc/git/i3_thuan5/語料/華', ],
 		['/home/Ihc/git/i3_thuan5/語料/閩', ],
 		['/home/Ihc/git/i3_thuan5/語料/閩全'],
-		3,
 		os.path.join(os.path.dirname(__file__), '暫存資料夾'),
+		連紲詞長度=3,
+		編碼器=語句編碼器(),
 		SRILM執行檔路徑='/usr/local/srilm/bin/i686-m64/',
 		GIZA執行檔路徑='/usr/local/mt/',
 		MOSES腳本路徑='/usr/local/mosesdecoder/scripts/',
