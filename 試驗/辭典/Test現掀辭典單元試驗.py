36548745ba477635269d22e92690d3a963c68eee
薛丞宏 <ihcaoe@gmail.com>
2015-05-30 10:26:44 +0800

    試驗先拆開 #103

--- 試驗/辭典/Test現掀辭典單元試驗.py
+++ 試驗/辭典/Test現掀辭典單元試驗.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-from 臺灣言語工具.表單.現掀辭典 import 現掀辭典
-from 試驗.表單.辭典單元試驗 import 辭典單元試驗
 from unittest.case import TestCase
 
+
+from 臺灣言語工具.表單.現掀辭典 import 現掀辭典
+from 試驗.辭典.辭典單元試驗 import 辭典單元試驗
 
 class 現掀辭典單元試驗(辭典單元試驗,TestCase):
 	辭典型態=現掀辭典
