5fc9add3f2dc3373eebdc796b4fc902765ea17e3
薛丞宏 <ihcaoe@gmail.com>
2015-05-30 11:21:55 +0800

    加kenlm訓練的試驗，而且正規化 #85

--- 試驗/語言模型/TestKenLM語言模型單元試驗.py
+++ 試驗/語言模型/TestKenLM語言模型單元試驗.py
@@ -13,7 +13,7 @@ from 臺灣言語工具.語言模型.KenLM語言模型 import KenLM語言模型
 機率=P(丙), P(乙丙), P(甲乙丙)
 條件=P(丙), P(乙丙)/P(乙), P(甲乙丙)/P(甲乙)
 '''
-class KenLM語言模型訓練單元試驗(TestCase):
+class KenLM語言模型單元試驗(TestCase):
 	忍受 = 1e-7
 	def setUp(self):
 		self.分析器 = 拆文分析器()
