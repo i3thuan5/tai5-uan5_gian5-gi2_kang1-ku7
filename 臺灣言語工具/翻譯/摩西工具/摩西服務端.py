7fb03e8f8fc75b624d3ab1889cefa24e3b250f77
薛丞宏 <ihcaoe@gmail.com>
2015-06-03 09:41:51 +0800

    修正摩西服務端的試驗

--- 臺灣言語工具/翻譯/摩西工具/摩西服務端.py
+++ 臺灣言語工具/翻譯/摩西工具/摩西服務端.py
@@ -18,12 +18,12 @@ class 摩西服務端():
 		self.模型路徑 = os.path.join(moses模型資料夾路徑, 'model', 'moses.ini')
 		if not os.path.isfile(self.模型路徑):
 			raise OSError('{0}模型無存在！！'.format(self.模型路徑))
+		self.埠=埠
 		self.程序 = None
 	def 走(self):
 		if not self.程序:
 			self.程序 = Popen(
 					[self.執行程式, '-f', self.模型路徑, '--server-port', str(self.埠)],
-					shell=True
 				)
 	def 停(self):
 		self.程序.terminate()
