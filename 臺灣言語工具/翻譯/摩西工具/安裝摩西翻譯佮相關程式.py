16f84565aa7ecef6ecc1c406b2ccdd39d483e896
薛丞宏 <ihcaoe@gmail.com>
2015-09-01 18:07:11 +0800

    mgiza配合moses的train-model.perl，複製py到bin fix #157

--- 臺灣言語工具/翻譯/摩西工具/安裝摩西翻譯佮相關程式.py
+++ 臺灣言語工具/翻譯/摩西工具/安裝摩西翻譯佮相關程式.py
@@ -2,7 +2,7 @@
 from 臺灣言語工具.系統整合.程式腳本 import 程式腳本
 from os import makedirs
 from os.path import join, isdir
-from shutil import move
+from shutil import move, copyfile
 
 
 from 臺灣言語工具.系統整合.外部程式 import 外部程式
@@ -63,3 +63,7 @@ class 安裝摩西翻譯佮相關程式(程式腳本):
             self._走指令(['cmake', '.'])
             self._走指令('make', 愛直接顯示輸出=True)
             self._走指令(['make', 'install'], 愛直接顯示輸出=True)
+            copyfile(
+                join('scripts', 'merge_alignment.py'),
+                join('bin', 'merge_alignment.py')
+            )
