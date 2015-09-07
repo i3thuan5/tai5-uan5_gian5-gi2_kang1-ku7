8cd928e9e7af911cf5d823057e7ba3f34decb435
薛丞宏 <ihcaoe@gmail.com>
2015-09-07 15:17:01 +0800

    補設定值 #166

--- 文件/conf.py
+++ 文件/conf.py
@@ -114,7 +114,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-# html_theme = 'default'
+html_theme = 'default'
 
 
 # Theme options are theme-specific and customize the look and feel of a theme
