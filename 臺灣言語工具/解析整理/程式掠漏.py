726baeb97f346a0c9725a6e9b0d9a037b229b7d9
薛丞宏 <ihcaoe@gmail.com>
2015-12-03 11:27:56 +0800

    系統整合佮解析整理的函式先改做靜態

--- 臺灣言語工具/解析整理/程式掠漏.py
+++ 臺灣言語工具/解析整理/程式掠漏.py
@@ -14,40 +14,48 @@ from 臺灣言語工具.解析整理.型態錯誤 import 型態錯誤
 
 
 class 程式掠漏:
-    def 毋是字物件就毋著(self, 字物件):
+    @classmethod
+    def 毋是字物件就毋著(cls, 字物件):
         if not isinstance(字物件, 字):
             raise 型態錯誤('傳入來的毋是字物件：{0},{1}'
                        .format(type(字物件), str(字物件)))
 
-    def 毋是詞物件就毋著(self, 詞物件):
+    @classmethod
+    def 毋是詞物件就毋著(cls, 詞物件):
         if not isinstance(詞物件, 詞):
             raise 型態錯誤('傳入來的毋是詞物件：{0},{1}'
                        .format(type(詞物件), str(詞物件)))
 
-    def 毋是組物件就毋著(self, 組物件):
+    @classmethod
+    def 毋是組物件就毋著(cls, 組物件):
         if not isinstance(組物件, 組):
             raise 型態錯誤('傳入來的毋是組物件：{0},{1}'
                        .format(type(組物件), str(組物件)))
 
-    def 毋是集物件就毋著(self, 集物件):
+    @classmethod
+    def 毋是集物件就毋著(cls, 集物件):
         if not isinstance(集物件, 集):
             raise 型態錯誤('傳入來的毋是集物件：{0},{1}'
                        .format(type(集物件), str(集物件)))
 
-    def 毋是句物件就毋著(self, 句物件):
+    @classmethod
+    def 毋是句物件就毋著(cls, 句物件):
         if not isinstance(句物件, 句):
             raise 型態錯誤('傳入來的毋是句物件：{0},{1}'
                        .format(type(句物件), str(句物件)))
 
-    def 毋是章物件就毋著(self, 章物件):
+    @classmethod
+    def 毋是章物件就毋著(cls, 章物件):
         if not isinstance(章物件, 章):
             raise 型態錯誤('傳入來的毋是章物件：{0},{1}'
                        .format(type(章物件), str(章物件)))
 
-    def 毋是字詞組集句章的毋著(self, 物件):
+    @classmethod
+    def 毋是字詞組集句章的毋著(cls, 物件):
         raise 型態錯誤('傳入來的毋是字詞組集句章其中一種物件：{0}，{1}'
                    .format(type(物件), str(物件)))
 
-    def 毋是字串都毋著(self, 語句):
+    @classmethod
+    def 毋是字串都毋著(cls, 語句):
         if not isinstance(語句, str):
             raise 型態錯誤('傳入來的語句毋是字串：{0}'.format(str(語句)))
--- 臺灣言語工具/解析整理/轉物件音家私.py
+++ 臺灣言語工具/解析整理/轉物件音家私.py
@@ -13,7 +13,8 @@ from 臺灣言語工具.基本元素.公用變數 import 標點符號
 
 class 轉物件音家私():
     # 逐个函式攏愛產生新的物件
-    def _轉字音(self, 音標工具, 字物件, 函式):
+    @classmethod
+    def _轉字音(cls, 音標工具, 字物件, 函式):
         if not isinstance(字物件, 字):
             raise 型態錯誤('傳入來的毋是字物件：{0}'.format(str(字物件)))
         if 字物件.型 in 標點符號 and 字物件.音 in 標點符號:
@@ -38,58 +39,64 @@ class 轉物件音家私():
             新型 = 字物件.型
         return 字(新型, 新音)
 
-    def _轉詞音(self, 音標工具, 詞物件, 函式):
+    @classmethod
+    def _轉詞音(cls, 音標工具, 詞物件, 函式):
         if not isinstance(詞物件, 詞):
             raise 型態錯誤('傳入來的毋是詞物件：{0}'.format(str(詞物件)))
         新詞物件 = 詞()
         for 字物件 in 詞物件.內底字:
-            新詞物件.內底字.append(self._轉字音(音標工具, 字物件, 函式))
+            新詞物件.內底字.append(cls._轉字音(音標工具, 字物件, 函式))
         return 新詞物件
 
-    def _轉組音(self, 音標工具, 組物件, 函式):
+    @classmethod
+    def _轉組音(cls, 音標工具, 組物件, 函式):
         if not isinstance(組物件, 組):
             raise 型態錯誤('傳入來的毋是組物件：{0}'.format(str(組物件)))
         新組物件 = 組()
         for 詞物件 in 組物件.內底詞:
-            新組物件.內底詞.append(self._轉詞音(音標工具, 詞物件, 函式))
+            新組物件.內底詞.append(cls._轉詞音(音標工具, 詞物件, 函式))
         return 新組物件
 
-    def _轉集音(self, 音標工具, 集物件, 函式):
+    @classmethod
+    def _轉集音(cls, 音標工具, 集物件, 函式):
         if not isinstance(集物件, 集):
             raise 型態錯誤('傳入來的毋是集物件：{0}'.format(str(集物件)))
         新集物件 = 集()
         for 組物件 in 集物件.內底組:
-            新集物件.內底組.append(self._轉組音(音標工具, 組物件, 函式))
+            新集物件.內底組.append(cls._轉組音(音標工具, 組物件, 函式))
         return 新集物件
 
-    def _轉句音(self, 音標工具, 句物件, 函式):
+    @classmethod
+    def _轉句音(cls, 音標工具, 句物件, 函式):
         if not isinstance(句物件, 句):
             raise 型態錯誤('傳入來的毋是句物件：{0}'.format(str(句物件)))
         新句物件 = 句()
         for 集物件 in 句物件.內底集:
-            新句物件.內底集.append(self._轉集音(音標工具, 集物件, 函式))
+            新句物件.內底集.append(cls._轉集音(音標工具, 集物件, 函式))
         return 新句物件
 
-    def _轉章音(self, 音標工具, 章物件, 函式):
+    @classmethod
+    def _轉章音(cls, 音標工具, 章物件, 函式):
         if not isinstance(章物件, 章):
             raise 型態錯誤('傳入來的毋是章物件：{0}'.format(str(章物件)))
         新章物件 = 章()
         for 句物件 in 章物件.內底句:
-            新章物件.內底句.append(self._轉句音(音標工具, 句物件, 函式))
+            新章物件.內底句.append(cls._轉句音(音標工具, 句物件, 函式))
         return 新章物件
 
-    def 轉音(self, 音標工具, 物件, 函式='預設音標'):
+    @classmethod
+    def 轉音(cls, 音標工具, 物件, 函式='預設音標'):
         if isinstance(物件, 字):
-            return self._轉字音(音標工具, 物件, 函式)
+            return cls._轉字音(音標工具, 物件, 函式)
         if isinstance(物件, 詞):
-            return self._轉詞音(音標工具, 物件, 函式)
+            return cls._轉詞音(音標工具, 物件, 函式)
         if isinstance(物件, 組):
-            return self._轉組音(音標工具, 物件, 函式)
+            return cls._轉組音(音標工具, 物件, 函式)
         if isinstance(物件, 集):
-            return self._轉集音(音標工具, 物件, 函式)
+            return cls._轉集音(音標工具, 物件, 函式)
         if isinstance(物件, 句):
-            return self._轉句音(音標工具, 物件, 函式)
+            return cls._轉句音(音標工具, 物件, 函式)
         if isinstance(物件, 章):
-            return self._轉章音(音標工具, 物件, 函式)
+            return cls._轉章音(音標工具, 物件, 函式)
         raise 型態錯誤('傳入來的毋是字詞組集句章其中一種物件：{0}，{1}'
                    .format(type(物件), str(物件)))
