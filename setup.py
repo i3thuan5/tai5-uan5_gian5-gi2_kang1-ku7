ad521a2057edd505b7315b26bada66965aac1037
薛丞宏 <ihcaoe@gmail.com>
2015-12-15 10:14:29 +0800

    setup自動加--format=zip的參數。版本0.5.11

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
-'''
-tar無法度下傷長的檔案名，所以愛用zip
-python setup.py sdist --format=zip upload
-'''
-import os
 from distutils.core import setup
+import os
+import sys
 from 版本 import 版本
 
 
@@ -15,6 +12,10 @@ def 揣工具包(頭='.'):
         if '__init__.py' in 檔案:
             工具包.append(目錄.replace('/', '.'))
     return 工具包
+
+# tar無法度下傷長的檔案名，所以愛用zip
+# python setup.py sdist --format=zip upload
+sys.argv.insert(sys.argv.index('upload'), '--format=zip')
 
 setup(
     # 臺灣言語工具 tai5_uan5_gian5_gi2_kang1_ku7
