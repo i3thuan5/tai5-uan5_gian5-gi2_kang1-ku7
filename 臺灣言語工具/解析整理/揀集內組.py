feb4a6e78ea076495d09ce271ce31887a0f6e24d
薛丞宏 <ihcaoe@gmail.com>
2015-12-24 19:48:50 +0800

    整理揀集內組，會當配合集內組照排，凊彩揀一個會當用的。 因為kaxabu辭典愛用著，所以先來整理

--- 臺灣言語工具/解析整理/揀集內組.py
+++ 臺灣言語工具/解析整理/揀集內組.py
@@ -3,30 +3,42 @@
 from 臺灣言語工具.基本元素.集 import 集
 from 臺灣言語工具.基本元素.句 import 句
 from 臺灣言語工具.基本元素.章 import 章
+from itertools import repeat
+from 臺灣言語工具.解析整理.解析錯誤 import 解析錯誤
 
 
 class 揀集內組:
 
     @classmethod
-    def 揀(cls, 章物件, 集選擇):
+    def 揀(cls, 物件, 集選擇=repeat(0)):
+        集選擇指標 = iter(集選擇)
+        if isinstance(物件, 章):
+            return cls._揀章物件(物件, 集選擇指標)
+        if isinstance(物件, 句):
+            return cls._揀句物件(物件, 集選擇指標)
+        return cls._揀集物件(物件, 集選擇指標)
+
+    @classmethod
+    def _揀章物件(cls, 章物件, 集選擇指標):
         揀出來的章物件 = 章()
-        所在 = 0
         for 句物件 in 章物件.內底句:
-            集陣列 = []
-            for 集物件 in 句物件.內底集:
-                if 所在 < len(集選擇):
-                    選擇 = 集選擇[所在]
-                else:
-                    選擇 = 0
-                所在 += 1
-                if 選擇 < len(集物件.內底組):
-                    組陣列 = 集物件.內底組[選擇:選擇 + 1]
-                else:
-                    組陣列 = 集物件.內底組[:1]
-                集物件 = 集()
-                集物件.內底組 = 組陣列
-                集陣列.append(集物件)
-            句物件 = 句()
-            句物件.內底集 = 集陣列
-            揀出來的章物件.內底句.append(句物件)
+            揀出來的章物件.內底句.append(cls._揀句物件(句物件, 集選擇指標))
         return 揀出來的章物件
+
+    @classmethod
+    def _揀句物件(cls, 句物件, 集選擇指標):
+        集陣列 = []
+        for 集物件 in 句物件.內底集:
+            集陣列.append(cls._揀集物件(集物件, 集選擇指標))
+        句物件 = 句()
+        句物件.內底集 = 集陣列
+        return 句物件
+
+    @classmethod
+    def _揀集物件(cls, 集物件, 集選擇指標):
+        選擇 = next(集選擇指標)
+        try:
+            組物件 = 集物件.內底組[選擇]
+        except IndexError:
+            raise 解析錯誤('集物件內底數量：{}，選擇位置：{}'.format(len(集物件.內底組), 選擇))
+        return 集([組物件])
