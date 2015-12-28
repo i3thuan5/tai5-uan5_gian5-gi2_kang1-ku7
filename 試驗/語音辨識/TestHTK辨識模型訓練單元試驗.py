b07040f0ebaa4db62c9b0b99c2f7c9ecc0c26054
薛丞宏 <ihcaoe@gmail.com>
2015-12-28 13:45:11 +0800

    親像kaxabu辭典的音檔攏點外鐘長，訓練初步模型需要prune較濟可能。 若一開始的參數無法度走，就較門檻調高，可能路線切較濟掉

--- 試驗/語音辨識/TestHTK辨識模型訓練單元試驗.py
+++ 試驗/語音辨識/TestHTK辨識模型訓練單元試驗.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
-from unittest.case import TestCase
 import os
+from os.path import join
+from tempfile import mkdtemp
+from unittest.case import TestCase
 from 臺灣言語工具.語音辨識.HTK工具.HTK辨識模型訓練 import HTK辨識模型訓練
+from 臺灣言語工具.系統整合.外部程式 import 外部程式
+from unittest.mock import patch
 
 
 class HTK辨識模型訓練單元試驗(TestCase):
@@ -23,3 +27,43 @@ class HTK辨識模型訓練單元試驗(TestCase):
             HTK辨識模型訓練._讀檔案(加短恬的模型)
         )
         os.remove(試加短恬的模型)
+
+    @patch('shutil.copy')
+    @patch('臺灣言語工具.系統整合.程式腳本.程式腳本._走指令')
+    def test_模型重估調整參數失敗一擺了成功(self, 走指令mock,複製檔案mock):
+        暫時資料夾 = mkdtemp()
+        暫時檔案 = join(暫時資料夾, '暫時檔案')
+        模型所在 = join(暫時資料夾, '模型.macro')
+        走指令mock.side_effect = [RuntimeError(), None, None]
+
+        HTK辨識模型訓練._模型重估(
+            外部程式.htk預設目錄(), 暫時資料夾, 暫時檔案, 暫時檔案, 暫時檔案, 模型所在, 1
+        )
+        self.assertEqual(走指令mock.call_count, 2)
+
+    @patch('shutil.copy')
+    @patch('臺灣言語工具.系統整合.程式腳本.程式腳本._走指令')
+    def test_模型重估調整參數失敗兩擺了成功(self, 走指令mock,複製檔案mock):
+        暫時資料夾 = mkdtemp()
+        暫時檔案 = join(暫時資料夾, '暫時檔案')
+        模型所在 = join(暫時資料夾, '模型.macro')
+        走指令mock.side_effect = [RuntimeError(), RuntimeError(), None]
+
+        HTK辨識模型訓練._模型重估(
+            外部程式.htk預設目錄(), 暫時資料夾, 暫時檔案, 暫時檔案, 暫時檔案, 模型所在, 1
+        )
+        self.assertEqual(走指令mock.call_count, 3)
+
+    @patch('shutil.copy')
+    @patch('臺灣言語工具.系統整合.程式腳本.程式腳本._走指令')
+    def test_模型重估調整參數無效三擺攏失敗(self, 走指令mock,複製檔案mock):
+        暫時資料夾 = mkdtemp()
+        暫時檔案 = join(暫時資料夾, '暫時檔案')
+        模型所在 = join(暫時資料夾, '模型.macro')
+        走指令mock.side_effect = [RuntimeError(), RuntimeError(), RuntimeError()]
+
+        with self.assertRaises(RuntimeError):
+            HTK辨識模型訓練._模型重估(
+                外部程式.htk預設目錄(), 暫時資料夾, 暫時檔案, 暫時檔案, 暫時檔案, 模型所在, 1
+            )
+        self.assertEqual(走指令mock.call_count, 3)
