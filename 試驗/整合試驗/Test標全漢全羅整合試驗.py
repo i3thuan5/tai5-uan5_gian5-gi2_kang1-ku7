7c0b698cdd4ac744764b866da2c4d495cbc521fa
薛丞宏 <ihcaoe@gmail.com>
2016-01-05 13:10:35 +0800

    工具改用靜態函式

--- 試驗/整合試驗/Test標全漢全羅整合試驗.py
+++ 試驗/整合試驗/Test標全漢全羅整合試驗.py
@@ -1,9 +1,7 @@
+import os
+from unittest.case import TestCase
 from 臺灣言語工具.音標系統.閩南語.臺灣閩南語羅馬字拼音 import 臺灣閩南語羅馬字拼音
 from 臺灣言語工具.解析整理.文章粗胚 import 文章粗胚
-import os
-import unittest
-
-
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
 from 臺灣言語工具.斷詞.拄好長度辭典揣詞 import 拄好長度辭典揣詞
 from 臺灣言語工具.斷詞.語言模型揀集內組 import 語言模型揀集內組
@@ -13,14 +11,7 @@ from 臺灣言語工具.語言模型.KenLM語言模型 import KenLM語言模型
 from 臺灣言語工具.辭典.型音辭典 import 型音辭典
 
 
-class 標全漢全羅整合試驗(unittest.TestCase):
-
-    def setUp(self):
-        self.揣詞 = 拄好長度辭典揣詞()
-        self.揀集內組 = 語言模型揀集內組()
-
-    def tearDown(self):
-        pass
+class 標全漢全羅整合試驗(TestCase):
 
     def test_字串辭典斷詞得著音(self):
         閩南語辭典 = 型音辭典(2)
@@ -37,8 +28,8 @@ class 標全漢全羅整合試驗(unittest.TestCase):
         處理減號 = 文章粗胚.建立物件語句前處理減號(臺灣閩南語羅馬字拼音, 閩南語語句)
         章物件 = 拆文分析器.建立章物件(處理減號)
         標準章物件 = 轉物件音家私.轉音(臺灣閩南語羅馬字拼音, 章物件)
-        斷詞章物件, _, _ = self.揣詞.揣詞(閩南語辭典, 標準章物件)
-        選好章物件, _, _ = self.揀集內組.揀(閩南語語言模型, 斷詞章物件)
+        斷詞章物件, _, _ = 拄好長度辭典揣詞.揣詞(閩南語辭典, 標準章物件)
+        選好章物件, _, _ = 語言模型揀集內組.揀(閩南語語言模型, 斷詞章物件)
 
         self.assertEqual(物件譀鏡.看音(選好章物件), 'gua2 ai3 a1-sui2')
         self.assertEqual(物件譀鏡.看型(選好章物件), '我愛阿媠')
