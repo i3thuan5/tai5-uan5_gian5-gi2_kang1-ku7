d1a431cebd1df9f0b46ce7be05e767526dfc8f7c
薛丞宏 <ihcaoe@gmail.com>
2016-01-11 15:59:51 +0800

    換主題，而且有法度正常上傳矣

--- 文件/conf.py
+++ 文件/conf.py
@@ -114,8 +114,19 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+# html_theme = 'default'
 
+import os
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+# readthedocs.org uses their theme by default, so no need to specify it
+if on_rtd:
+    html_theme = 'default'
+# only import and set the theme if we're building docs locally
+else:
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
