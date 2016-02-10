538ab28116d76743cfd5fea0056c7703bd823e3b
薛丞宏 <ihcaoe@gmail.com>
2016-02-10 17:59:30 +0800

    整理語言綜合標音的試驗，主要攏愛予輸出會當正常顯示

--- 試驗/音標系統/Test官話綜合標音單元試驗.py
+++ 試驗/音標系統/Test官話綜合標音單元試驗.py
@@ -1,69 +1,44 @@
 # -*- coding: utf-8 -*-
-import unittest
+from unittest.case import TestCase
 from 臺灣言語工具.基本物件.字 import 字
-from 臺灣言語工具.綜合標音.官話字綜合標音 import 官話字綜合標音
+from 臺灣言語工具.綜合標音.官話綜合標音 import 官話綜合標音
 from 臺灣言語工具.基本物件.公用變數 import 無音
 from 臺灣言語工具.解析整理.型態錯誤 import 型態錯誤
-from 臺灣言語工具.解析整理.解析錯誤 import 解析錯誤
 
 
-class 官話綜合標音單元試驗(unittest.TestCase):
-    def setUp(self):
-        self.我型 = '我'
-        self.我音 = 'ㄨㄛˇ'
-        self.我標音 = '⿿⿿ㄨㄛˇ'
-        self.你型 = '你'
-        self.你音 = 'ㄋㄧˇ'
+class 官話綜合標音單元試驗(TestCase):
 
-    def tearDown(self):
-        pass
+    def test_對齊物件(self):
+        self.assertEqual(字('我', 'ㄨㄛˇ').綜合標音(官話綜合標音), {
+            "漢字": '我', "注音符號": 'ㄨㄛˇ'
+        })
 
-    def test_合法(self):
-        綜合標音 = 官話字綜合標音(字(self.我型, self.我音))
-        self.assertEqual(綜合標音.標音完整無(), True)
-        綜合標音 = 官話字綜合標音(字(self.我型, self.我音), False)
-        self.assertEqual(綜合標音.標音完整無(), True)
-        綜合標音 = 官話字綜合標音(字(self.我型, self.我音), True)
-        self.assertEqual(綜合標音.標音完整無(), True)
+    def test_無音綜合標音(self):
+        self.assertEqual(字('我', 無音).綜合標音(官話綜合標音), {
+            "漢字": '我', "注音符號": '我'
+        })
 
-    def test_兩个字合法(self):
-        我綜合標音 = 官話字綜合標音(字(self.我型, self.我音))
-        你綜合標音 = 官話字綜合標音(字(self.你型, self.你音))
-        self.assertEqual(我綜合標音.標音完整無(), True)
-        self.assertEqual(你綜合標音.標音完整無(), True)
-        self.assertEqual(我綜合標音.型體, self.我型)
-        self.assertEqual(你綜合標音.型體, self.你型)
+    def test_標點綜合標音(self):
+        self.assertEqual(字('，', 無音).綜合標音(官話綜合標音), {
+            "漢字": '，', "注音符號": '，'
+        })
 
-    def test_轉json格式(self):
-        綜合標音 = 官話字綜合標音(字(self.我型, self.我音))
-        self.assertEqual(綜合標音.轉json格式(),
-                         {"型體": self.我型, "注音符號": self.我標音})
-        self.assertEqual(綜合標音.標音完整無(), True)
+    def test_對齊標點綜合標音(self):
+        self.assertEqual(字('，', '.').綜合標音(官話綜合標音), {
+            "漢字": '，', "注音符號": '.'
+        })
 
-    def test_標點合法(self):
-        標點 = 官話字綜合標音(字('，', 無音))
-        self.assertEqual(標點.標音完整無(), True)
-
-    def test_標點轉json格式(self):
-        標點 = 官話字綜合標音(字('，', 無音))
-        self.assertEqual(標點.轉json格式(), {"型體": "，", "注音符號": ""})
-
-    def test_標點音無合法(self):
-        標音 = 官話字綜合標音(字('我', 'ㄆㄨㄧˋ'))
-        self.assertEqual(標音.標音完整無(), False)
-        標音 = 官話字綜合標音(字('我', 'ㄆㄨㄧˋ'), False)
-        self.assertEqual(標音.標音完整無(), False)
-        self.assertRaises(解析錯誤, 官話字綜合標音, 字('我', 'ㄆㄨㄧˋ'), True)
+    def test_標點音無合法的綜合標音(self):
+        self.assertEqual(字('我', 'ㄉㄜ˙').綜合標音(官話綜合標音), {
+            "漢字": '我', "注音符號": 'ㄆㄨㄧˋ'
+        })
 
     def test_烏白傳(self):
-        self.assertRaises(型態錯誤, 官話字綜合標音, '我')
-        self.assertRaises(型態錯誤, 官話字綜合標音, '我', False)
-        self.assertRaises(型態錯誤, 官話字綜合標音, '我', True)
+        self.assertRaises(型態錯誤, 官話綜合標音, '我')
+        self.assertRaises(型態錯誤, 官話綜合標音, '我', False)
+        self.assertRaises(型態錯誤, 官話綜合標音, '我', True)
 
     def test_輕聲轉json格式(self):
-        型 = '的'
-        音 = 'ㄉㄜ˙'
-        綜合標音 = 官話字綜合標音(字(型, 音))
-        self.assertEqual(綜合標音.標音完整無(), True)
-        self.assertEqual(綜合標音.轉json格式(),
-                         {"型體": 型, "注音符號": '⿿⿿˙ㄉㄜ'})
+        self.assertEqual(字('的', 'ㄉㄜ˙').綜合標音(官話綜合標音), {
+            "漢字": '的', "注音符號": 'ㄉㄜ˙'
+        })
--- 試驗/音標系統/Test客話綜合標音單元試驗.py
+++ 試驗/音標系統/Test客話綜合標音單元試驗.py
@@ -1,60 +1,44 @@
 # -*- coding: utf-8 -*-
-import unittest
+from unittest.case import TestCase
 from 臺灣言語工具.基本物件.字 import 字
-from 臺灣言語工具.綜合標音.客話字綜合標音 import 客話字綜合標音
+from 臺灣言語工具.綜合標音.客話綜合標音 import 客話綜合標音
 from 臺灣言語工具.基本物件.公用變數 import 無音
 from 臺灣言語工具.解析整理.型態錯誤 import 型態錯誤
-from 臺灣言語工具.解析整理.解析錯誤 import 解析錯誤
 
 
-class 客話綜合標音單元試驗(unittest.TestCase):
-    def setUp(self):
-        self.我型 = '⿰厓'
-        self.我音 = 'ngaiˇ'
-        self.你型 = '你'
-        self.你音 = 'niˇ'
+class 客話綜合標音單元試驗(TestCase):
 
-    def tearDown(self):
-        pass
+    def test_對齊物件(self):
+        self.assertEqual(字('⿰厓', 'ngaiˇ').綜合標音(客話綜合標音), {
+            "型體": '⿰厓',
+            "臺灣客話": 'ngaiˇ'
+        })
 
-    def test_合法(self):
-        綜合標音 = 客話字綜合標音(字(self.我型, self.我音))
-        self.assertEqual(綜合標音.標音完整無(), True)
-        綜合標音 = 客話字綜合標音(字(self.我型, self.我音), False)
-        self.assertEqual(綜合標音.標音完整無(), True)
-        綜合標音 = 客話字綜合標音(字(self.我型, self.我音), True)
-        self.assertEqual(綜合標音.標音完整無(), True)
+    def test_無音物件(self):
+        self.assertEqual(字('⿰厓', 無音).綜合標音(客話綜合標音), {
+            "型體": '⿰厓',
+            "臺灣客話": '⿰厓'
+        })
 
-    def test_兩个字合法(self):
-        我綜合標音 = 客話字綜合標音(字(self.我型, self.我音))
-        你綜合標音 = 客話字綜合標音(字(self.你型, self.你音))
-        self.assertEqual(我綜合標音.標音完整無(), True)
-        self.assertEqual(你綜合標音.標音完整無(), True)
-        self.assertEqual(我綜合標音.型體, self.我型)
-        self.assertEqual(你綜合標音.型體, self.你型)
+    def test_標點物件(self):
+        self.assertEqual(字('，', 無音).綜合標音(客話綜合標音), {
+            "型體": '，',
+            "臺灣客話": '，'
+        })
 
-    def test_轉json格式(self):
-        綜合標音 = 客話字綜合標音(字(self.我型, self.我音))
-        self.assertEqual(綜合標音.標音完整無(), True)
-        self.assertEqual(綜合標音.轉json格式(),
-                         {"型體": self.我型, "臺灣客話": self.我音})
-
-    def test_標點合法(self):
-        標點 = 客話字綜合標音(字('，', 無音))
-        self.assertEqual(標點.標音完整無(), True)
-
-    def test_標點轉json格式(self):
-        標點 = 客話字綜合標音(字('，', 無音))
-        self.assertEqual(標點.轉json格式(), {"型體": "，", "臺灣客話": ""})
+    def test_對齊標點物件(self):
+        self.assertEqual(字('，', ',').綜合標音(客話綜合標音), {
+            "型體": '，',
+            "臺灣客話": ','
+        })
 
     def test_標點音無合法(self):
-        綜合標音 = 客話字綜合標音(字('我', 'nggai'))
-        self.assertEqual(綜合標音.標音完整無(), False)
-        綜合標音 = 客話字綜合標音(字('我', 'nggai'), False)
-        self.assertEqual(綜合標音.標音完整無(), False)
-        self.assertRaises(解析錯誤, 客話字綜合標音, 字('我', 'nggai'), True)
+        self.assertEqual(字('我', 'nggai').綜合標音(客話綜合標音), {
+            "型體": '我',
+            "臺灣客話": 'nggai'
+        })
 
     def test_烏白傳(self):
-        self.assertRaises(型態錯誤, 客話字綜合標音, '我')
-        self.assertRaises(型態錯誤, 客話字綜合標音, '我', False)
-        self.assertRaises(型態錯誤, 客話字綜合標音, '我', True)
+        self.assertRaises(型態錯誤, 客話綜合標音, '我')
+        self.assertRaises(型態錯誤, 客話綜合標音, '我', False)
+        self.assertRaises(型態錯誤, 客話綜合標音, '我', True)
