545dc9da0548c8a841cd1396abf214f827c2ddb8
薛丞宏 <ihcaoe@gmail.com>
2016-11-01 13:04:17 +0800

    予「｜｜｜」的分詞會當正常處理掉

--- 試驗/解析整理/Test拆文分析器分詞單元試驗.py
+++ 試驗/解析整理/Test拆文分析器分詞單元試驗.py
@@ -130,6 +130,24 @@ class 拆文分析器分詞單元試驗(unittest.TestCase):
         self.assertEqual(詞物件.內底字[0].型, 型)
         self.assertEqual(詞物件.內底字[0].音, 音)
 
+    def test_分詞全是分詞符號(self):
+        詞物件 = 拆文分析器.分詞詞物件('｜｜｜')
+        self.assertEqual(len(詞物件.內底字), 1)
+        self.assertEqual(詞物件.內底字[0].型, '｜')
+        self.assertEqual(詞物件.內底字[0].音, '｜')
+
+    def test_分詞型是分詞符號(self):
+        詞物件 = 拆文分析器.分詞詞物件('｜｜=')
+        self.assertEqual(len(詞物件.內底字), 1)
+        self.assertEqual(詞物件.內底字[0].型, '｜')
+        self.assertEqual(詞物件.內底字[0].音, '=')
+
+    def test_分詞音是分詞符號(self):
+        詞物件 = 拆文分析器.分詞詞物件('-｜｜')
+        self.assertEqual(len(詞物件.內底字), 1)
+        self.assertEqual(詞物件.內底字[0].型, '-')
+        self.assertEqual(詞物件.內底字[0].音, '｜')
+
     def test_分詞組集句章孤字(self):
         分詞 = '𪜶｜in1'
         組物件 = 拆文分析器.分詞組物件(分詞)
