0c97aee49c89bceffb75ed9281bee599d21a1315
fafoy <ihcaoe@gmail.com>
2016-11-30 06:20:47 +0800

    重排版

--- 臺灣言語工具/解析整理/文章粗胚.py
+++ 臺灣言語工具/解析整理/文章粗胚.py
@@ -8,7 +8,6 @@ from 臺灣言語工具.解析整理.解析錯誤 import 解析錯誤
 from 臺灣言語工具.基本物件.公用變數 import 組字式符號
 from 臺灣言語工具.基本物件.公用變數 import 統一碼漢字佮組字式類
 from 臺灣言語工具.基本物件.公用變數 import 統一碼羅馬字類
-from 臺灣言語工具.基本物件.公用變數 import 統一碼數字類
 from 臺灣言語工具.基本物件.公用變數 import 聲調符號
 import re
 
