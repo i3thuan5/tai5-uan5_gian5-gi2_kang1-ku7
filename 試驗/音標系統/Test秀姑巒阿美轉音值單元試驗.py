dbeef744e4f256bf3c4b887bcadc2d98de29ed73
薛丞宏 <ihcaoe@gmail.com>
2017-01-16 13:12:27 +0800

    詞頭詞尾若是元音，愛補咽喉塞音 #341

--- 試驗/音標系統/Test秀姑巒阿美轉音值單元試驗.py
+++ 試驗/音標系統/Test秀姑巒阿美轉音值單元試驗.py
@@ -7,12 +7,27 @@ class 原住民族語言書寫系統秀姑巒阿美語轉音值單元試驗(Test
 
     def test_一般(self):
         self.assertEqual(
+            原住民族語言書寫系統秀姑巒阿美語("cidal").音值(),
+            [['ts', 'i'], ['ɬ', 'a', 'ɾ']]
+        )
+
+    def test_詞尾補塞音(self):
+        self.assertEqual(
             原住民族語言書寫系統秀姑巒阿美語("kako").音值(),
-            [['k', 'a'], ['k', 'o']]
+            [['k', 'a'], ['k', 'o', 'ʔ']]
+        )
+
+    def test_詞頭詞尾攏愛補(self):
+        self.assertEqual(
+            原住民族語言書寫系統秀姑巒阿美語('ina').音值(),
+            [['ʔ', 'i'], ['n', 'a', 'ʔ']]
         )
 
     def test_一个符號(self):
-        self.assertEqual(原住民族語言書寫系統秀姑巒阿美語("o").音值(), [['o']])
+        self.assertEqual(
+            原住民族語言書寫系統秀姑巒阿美語("o").音值(),
+            [['ʔ', 'o', 'ʔ']]
+        )
 
     def test_ng(self):
         self.assertEqual(
@@ -29,11 +44,14 @@ class 原住民族語言書寫系統秀姑巒阿美語轉音值單元試驗(Test
     def test_VGGV(self):
         self.assertEqual(
             原住民族語言書寫系統秀姑巒阿美語("wayya").音值(),
-            [['w', 'a', 'j'], ['j', 'a']]
+            [['w', 'a', 'j'], ['j', 'a', 'ʔ']]
         )
 
     def test_大寫(self):
-        self.assertEqual(原住民族語言書寫系統秀姑巒阿美語("O").音值(), [['o']])
+        self.assertEqual(
+            原住民族語言書寫系統秀姑巒阿美語("O").音值(),
+            [['ʔ', 'o', 'ʔ']]
+        )
 
     def test_預設音標就是家己(self):
         self.assertEqual(原住民族語言書寫系統秀姑巒阿美語("O").預設音標(), 'O')
