89b20f921053a906ac7ae47a6a8f627ad137c371
薛丞宏 <ihcaoe@gmail.com>
2017-04-30 17:51:27 +0800

    照劉老師的意見修試驗
    https://github.com/sih4sing5hong5/tai5-uan5_gian5-gi2_kang1-ku7/pull/380#issuecomment-298222438

--- 試驗/音標系統/Test賽夏轉音值單元試驗.py
+++ 試驗/音標系統/Test賽夏轉音值單元試驗.py
@@ -17,8 +17,20 @@ class 賽夏轉音值單元試驗(TestCase):
 
     def test_多音節(self):
         self.assertEqual(
+            賽夏("lomaliw").音值(),
+            [['l', 'o'], ['m', 'a'], ['l', 'i', 'w']]
+        )
+
+    def test_書寫CowV實際上是CwV(self):
+        self.assertEqual(
             賽夏("rowaSek").音值(),
-            [['r', 'o'], ['w', 'a'], ['ʃ', 'e', 'k']]
+            [['r', 'w', 'a'], ['ʃ', 'e', 'k']]
+        )
+
+    def test_書寫CiyV實際上是CyV(self):
+        self.assertEqual(
+            賽夏("ngiyaw").音值(),
+            [['ŋ', 'y', 'a', 'w']]
         )
 
     def test_喉塞尾(self):
@@ -31,6 +43,12 @@ class 賽夏轉音值單元試驗(TestCase):
         self.assertEqual(
             賽夏("hawaw").音值(),
             [['h', 'a'], ['w', 'a', 'w']]
+        )
+
+    def test_wow音節(self):
+        self.assertEqual(
+            賽夏("bawow").音值(),
+            [['b', 'a'], ['w', 'o', 'w']]
         )
 
     def test_雙字母的母音(self):
@@ -50,49 +68,69 @@ class 賽夏轉音值單元試驗(TestCase):
         self.assertIsNone(拼音.音標)
         self.assertEqual(拼音.音值(), [])
 
-    def test_長音後接子音(self):
+    def test_冒號後同元音代表同一音節_接子音(self):
         self.assertEqual(
             賽夏("ra:am").音值(),
-            [['r', 'a:'], ['a', 'm']]
+            [['r', 'a:', 'm']]
         )
 
-    def test_長音後接別音節子音(self):
-        self.assertEqual(
-            賽夏("ra:ma'").音值(),
-            [['r', 'a:'], ['m', 'a', 'ʔ']]
-        )
-
-    def test_長音後接元音(self):
-        self.assertEqual(
-            賽夏("ka:i'").音值(),
-            [['k', 'a:'], ['i', 'ʔ']]
-        )
-
-    def test_長音後音同樣元音(self):
+    def test_冒號後同元音代表同一音節_接滑音(self):
         self.assertEqual(
             賽夏("ka:ay").音值(),
             [['k', 'a:'], ['a', 'j']]
         )
 
-    def test_長音後接別的音節(self):
+    def test_冒號後同元音代表同一音節_接下一個音節的子音(self):
         self.assertEqual(
             賽夏("ka:anga'oraehan").音值(),
             [
-                ['k', 'a:'], ['a'], ['ŋ', 'a'],
+                ['k', 'a:'], ['ŋ', 'a'],
                 ['ʔ', 'o'], ['r', 'æ'], ['h', 'a', 'n']
             ]
+        )
+
+    def test_冒號後不同元音代表同一音節的滑音(self):
+        self.assertEqual(
+            賽夏("ka:i'").音值(),
+            [['k', 'a:', 'y', 'ʔ']]
+        )
+
+    def test_冒號後接子音代表和下一個音節中有停頓(self):
+        self.assertEqual(
+            賽夏("ra:ma'").音值(),
+            [['r', 'a', 'L'], ['m', 'a', 'ʔ']]
         )
 
     def test_多個長音(self):
         self.assertEqual(
             賽夏("hi:hi:hi:").音值(),
-            [['h', 'i:'], ['h', 'i:'], ['h', 'i:']]
+            [['h', 'i', 'L'], ['h', 'i', 'L'], ['h', 'i:']]
         )
 
-    def test_長雙字母元音(self):
+    def test_雙字母長元音(self):
         self.assertEqual(
             賽夏("'ae:ae:iw").音值(),
             [['ʔ', 'æ:'], ['æ:'], ['i', 'w']]
+        )
+
+    def test_長音後的iy尾(self):
+        '其他狀況下不會出現iy尾'
+        self.assertEqual(
+            賽夏("ra:iy").音值(),
+            [['r', 'a:', 'y']]
+        )
+
+    def test_長音後的元音是下一個音節的滑音(self):
+        'iy在長音後，也是代表一個滑音y'
+        self.assertEqual(
+            賽夏("rina:iyan").音值(),
+            [['r', 'i'], ['n', 'a:'], ['y', 'a', 'n']]
+        )
+
+    def test_加拉灣(self):
+        self.assertEqual(
+            賽夏("lala:ai'").音值(),
+            [['l', 'a'], ['l', 'a:'], ['i', 'ʔ']]
         )
 
     def test_預設音標就是家己(self):
