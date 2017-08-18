b692488ebc2ca8c4feb2cc01c6a1d9ea657311be
薛丞宏 <ihcaoe@gmail.com>
2017-08-18 13:01:49 +0800

    實作kenlm佮lmplz的選其中一个來走

--- 臺灣言語工具/語言模型/安裝KenLM訓練程式.py
+++ 臺灣言語工具/語言模型/安裝KenLM訓練程式.py
@@ -12,7 +12,7 @@ class 安裝KenLM訓練程式(安裝程式腳本):
     @classmethod
     def 安裝kenlm(cls, kenlm安裝路徑=外部程式.目錄(), 編譯CPU數=4):
         makedirs(kenlm安裝路徑, exist_ok=True)
-        kenlm程式碼目錄 = join(kenlm安裝路徑, 'KenLM')
+        kenlm程式碼目錄 = cls.kenlm資料夾路徑(kenlm安裝路徑)
         if not isdir(kenlm程式碼目錄):
             with cls._換目錄(kenlm安裝路徑):
                 cls._走指令([
@@ -27,3 +27,7 @@ class 安裝KenLM訓練程式(安裝程式腳本):
                 cls._更新專案()
         with cls._換目錄(kenlm程式碼目錄):
             cls._走指令(['./bjam', '-j{0}'.format(編譯CPU數)], 愛直接顯示輸出=True)
+
+    @classmethod
+    def kenlm資料夾路徑(cls, kenlm安裝路徑):
+        return join(kenlm安裝路徑, 'KenLM')
