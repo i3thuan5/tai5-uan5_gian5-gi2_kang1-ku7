64813602a03aacc4d300e5e9059ee60585e7ad0d
薛丞宏 <ihcaoe@gmail.com>
2017-12-08 18:33:21 +0800

    補日文的試驗

--- 試驗/解析整理/Test文章粗胚漢字中央加分字單元試驗.py
+++ 試驗/解析整理/Test文章粗胚漢字中央加分字單元試驗.py
@@ -20,3 +20,27 @@ class 文章粗胚處理減號單元試驗(TestCase):
             文章粗胚.漢字中央加分字符號(原來語句),
             處理好語句
         )
+
+    def test_hiragana(self):
+        原來語句 = 'アルミ'
+        處理好語句 = 'ア-ル-ミ'
+        self.assertEqual(
+            文章粗胚.漢字中央加分字符號(原來語句),
+            處理好語句
+        )
+
+    def test_katakana(self):
+        原來語句 = 'まんが'
+        處理好語句 = 'ま-ん-が'
+        self.assertEqual(
+            文章粗胚.漢字中央加分字符號(原來語句),
+            處理好語句
+        )
+
+    def test_hiragana佮漢字(self):
+        原來語句 = 'じゃん拳'
+        處理好語句 = 'じ-ゃ-ん-拳'
+        self.assertEqual(
+            文章粗胚.漢字中央加分字符號(原來語句),
+            處理好語句
+        )
