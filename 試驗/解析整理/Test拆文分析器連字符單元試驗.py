6c2e3f21dc8adf8f44ea8d959c8c7a14da034c90
pigu <ihcaoe@gmail.com>
2018-02-19 21:56:42 +0800

    整理連字符的全部情形

--- 試驗/解析整理/Test拆文分析器連字符單元試驗.py
+++ 試驗/解析整理/Test拆文分析器連字符單元試驗.py
@@ -1,32 +1,115 @@
 # -*- coding: utf-8 -*-
 from unittest.case import TestCase
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
-from 臺灣言語工具.基本物件.字 import 字
+from 臺灣言語工具.解析整理.解析錯誤 import 解析錯誤
 
 
 class 拆文分析器連字符單元試驗(TestCase):
-    def test_句頭輕聲無連做伙嘛會使(self):
-        型 = '-- 啊'
-        音 = '-- ah'
-        組物件 = 拆文分析器.對齊組物件(型, 音)
-        self.assertEqual(組物件.篩出字物件(), [
-            字('啊', '0ah'),
-        ])
+    def tearDown(self):
+        if self.錯誤:
+            with self.assertRaises(解析錯誤):
+                拆文分析器.建立句物件(self.句)
+        else:
+            self.assertEqual(
+                len(拆文分析器.建立句物件(self.句).篩出字物件()),
+                self.字數
+            )
 
-    def test_有處理減號的輕聲(self):
-        self.fail()
-        句 = '-tiong7-tiam2-'
-        組物件 = 拆文分析器.建立組物件(句)
-        self.assertTrue(組物件.篩出字物件()[-1].敢是輕聲())
+    def test_一sui2(self):
+        self.句 = '-sui2'
+        self.錯誤 = True
 
-    def test_有處理減號的輕聲(self):
-        self.fail()
-        句 = '--tsit8--puann3--'
-        組物件 = 拆文分析器.建立組物件(句)
-        self.assertTrue(組物件.篩出字物件()[-1].敢是輕聲())
+    def test_sui2一(self):
+        self.句 = 'sui2-'
+        self.錯誤 = True
 
-    def test_有處理減號的輕聲(self):
+    def test_sui2一sui2(self):
+        self.句 = 'sui2-sui2'
+        self.錯誤 = False
+        self.字數 = 2
+
+    def test_sui2一_sui2(self):
+        self.句 = 'sui2- sui2'
+        self.錯誤 = True
+
+    def test_sui2_一_sui2(self):
+        self.句 = 'sui2 - sui2'
+        self.錯誤 = False
+        self.字數 = 1 + 1 + 1
+
+    def test_sui2_一sui2(self):
+        self.句 = 'sui2 -sui2'
+        self.錯誤 = True
+
+    def test_一一sui2(self):
+        self.句 = '--sui2'
+        self.錯誤 = False
+        self.字數 = 1
+
+    def test_一一_sui2(self):
+        self.句 = '-- sui2'
+        self.錯誤 = False
+        self.字數 = 2 + 1
         self.fail()
-        句 = '---pun---khui---'
-        組物件 = 拆文分析器.建立組物件(句)
-        self.assertEqual(len(組物件.網出詞物件()), 3)
+
+    def test_一_一sui2(self):
+        self.句 = '- -sui2'
+        self.錯誤 = True
+
+    def test_sui2一一(self):
+        self.句 = 'sui2--'
+        self.錯誤 = True
+
+    def test_sui2一一sui2(self):
+        self.句 = 'sui2--sui2'
+        self.錯誤 = False
+        self.字數 = 2
+
+    def test_sui2一一_sui2(self):
+        self.句 = 'sui2-- sui2'
+        self.錯誤 = True
+
+    def test_sui2_一一_sui2(self):
+        self.句 = 'sui2 -- sui2'
+        self.錯誤 = True
+
+    def test_sui2_一一sui2(self):
+        self.句 = 'sui2 --sui2'
+        self.錯誤 = False
+        self.字數 = 2
+
+    def test_sui2一_一sui2(self):
+        self.句 = 'sui2- -sui2'
+        self.錯誤 = True
+
+    def test_一一一_sui2(self):
+        self.句 = '--- sui2'
+        self.錯誤 = False
+        self.字數 = 3 + 1
+
+    def test_一一一sui2(self):
+        self.句 = '---sui2'
+        self.錯誤 = True
+
+    def test_一_一一sui2(self):
+        self.句 = '- --sui2'
+        self.錯誤 = False
+        self.字數 = 1 + 1
+
+    def test_一一_一sui2(self):
+        self.句 = '-- -sui2'
+        self.錯誤 = True
+
+    def test_sui2一一一sui2(self):
+        self.句 = 'sui2---sui2'
+        self.錯誤 = True
+
+    def test_9_一_3_二_6(self):
+        self.句 = '9 - 3 = 6'
+        self.錯誤 = False
+        self.字數 = 5
+
+    def test_3_一_9_二_一6(self):
+        self.句 = '3 - 9 = -6'
+        self.錯誤 = False
+        self.字數 = 6
