c700596d81b286c1ed1af992598f0ad2d0d517e6
薛丞宏 <ihcaoe@gmail.com>
2018-03-02 12:14:38 +0800

    整理試驗

--- 試驗/解析整理/Test拆文分析器斷詞單元試驗.py
+++ 試驗/解析整理/Test拆文分析器斷詞單元試驗.py
@@ -4,37 +4,56 @@ from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
 
 
 class 拆文分析器斷詞單元試驗(TestCase):
+    def tearDown(self):
+        句物件 = 拆文分析器.建立句物件(self.語句)
+        self.assertEqual(len(句物件.網出詞物件()), self.詞數)
+
+    def test_全羅看有黏做伙無決定斷詞(self):
+        self.語句 = 'Guan2 tsit4-ma2'
+        self.詞數 = 2
+
+    def test_全羅輕聲看有黏做伙無決定斷詞(self):
+        self.語句 = 'Mi̍h-kiānn phah-bô--khì --ah'
+        self.詞數 = 3
+
+    def test_漢字看有黏做伙無決定斷詞(self):
+        self.語句 = '媠 姑娘'
+        self.詞數 = 2
+
+    def test_漢字輕聲就當作無仝詞(self):
+        self.語句 = '好 --矣'
+        self.詞數 = 2
+
     def test_漢字知影有輕聲猶原一個詞(self):
-        音 = '害--矣--啦'
-        組物件 = 拆文分析器.建立組物件(音)
-        self.assertEqual(len(組物件.網出詞物件()), 1)
+        self.語句 = '害--矣--啦'
+        self.詞數 = 1
 
     def test_句中輕聲無連做伙嘛會使(self):
-        漢 = '講會出--來'
-        組物件 = 拆文分析器.建立組物件(漢)
-        self.assertEqual(len(組物件.網出詞物件()), 1)
+        self.語句 = '講會出--來'
+        self.詞數 = 1
 
     def test_句中輕聲kah4後壁無連做伙嘛會使(self):
-        漢 = '講--出-來'
-        組物件 = 拆文分析器.建立組物件(漢)
-        self.assertEqual(len(組物件.網出詞物件()), 1)
-
-    def test_看有黏做伙決定斷詞(self):
-        音 = 'Mi̍h-kiānn phah-bô--khì --ah'
-        組物件 = 拆文分析器.建立組物件(音)
-        self.assertEqual(len(組物件.網出詞物件()), 3)
+        self.語句 = '講--出-來'
+        self.詞數 = 1
 
     def test_組字當作漢字(self):
-        句 = '癩⿸疒哥人'
-        組物件 = 拆文分析器.建立組物件(句)
-        self.assertEqual(len(組物件.網出詞物件()), 1)
+        self.語句 = '癩⿸疒哥人'
+
+        self.詞數 = 1
 
     def test_漢羅做伙(self):
-        句 = '台文通訊Bóng報'
-        組物件 = 拆文分析器.建立組物件(句)
-        self.assertEqual(len(組物件.網出詞物件()), 1)
+        self.語句 = '台文通訊Bóng報'
+
+        self.詞數 = 1
+
+    def test_漢羅輕聲(self):
+        self.語句 = '阿菊姨--a7'
+        self.詞數 = 1
+
+    def test_標點愛分開(self):
+        self.語句 = '「白話字」'
+        self.詞數 = 3
 
     def test_有連字符就認連字符(self):
-        組物件 = 拆文分析器.建立組物件('無-？-bo5-?')
-        self.assertEqual(len(組物件.內底詞), 1)
-        self.assertEqual(len(組物件.篩出字物件()), 4)
+        self.語句 = '無-？-bo5-?'
+        self.詞數 = 1
