6715500965c60ba1b22776b2618bbb719941d4db
pigu <ihcaoe@gmail.com>
2018-04-18 16:08:49 +0800

    feat: 跳過白話字數字調試驗，暫時不啟用。

--- 試驗/音標系統/閩南語/Test臺灣閩南語羅馬字拼音轉白話字數字調單元試驗.py
+++ 試驗/音標系統/閩南語/Test臺灣閩南語羅馬字拼音轉白話字數字調單元試驗.py
@@ -8,6 +8,7 @@ from 臺灣言語工具.音標系統.閩南語.臺灣閩南語羅馬字拼音 im
 from unittest.case import skip
 
 
+@skip('暫時取消該功能')
 class 臺羅轉白話字數字調單元試驗(unittest.TestCase):
 
     # 一律輸出小寫
@@ -44,6 +45,7 @@ class 臺羅轉白話字數字調單元試驗(unittest.TestCase):
         self.assertEqual(拼音物件.轉白話字數字調(), '0chui2')
 
     def test_雙母音oa(self):
+        self.assertEqual(臺灣閩南語羅馬字拼音('ua5').轉白話字數字調(), 'oa5')
         self.assertEqual(臺灣閩南語羅馬字拼音('uan5').轉白話字數字調(), 'oan5')
 
     def test_三母音oai(self):
@@ -52,17 +54,19 @@ class 臺羅轉白話字數字調單元試驗(unittest.TestCase):
     def test_三母音iau(self):
         self.assertEqual(臺灣閩南語羅馬字拼音('iau7').轉白話字數字調(), 'iau7')
 
-    def test_鼻化音(self):
+    def test_鼻化音nn(self):
         self.assertEqual(臺灣閩南語羅馬字拼音('ainn1').轉白話字數字調(), 'aiⁿ1')
 
-    def test_鼻化音_nnh(self):
+    def test_鼻化音nnh(self):
         self.assertEqual(臺灣閩南語羅馬字拼音('ainnh4').轉白話字數字調(), 'aihⁿ4')
 
+    def test_oo(self):
+        # ó͘, ō͘,
+        self.assertEqual(臺灣閩南語羅馬字拼音('hoo7').轉白話字數字調(), 'ho͘7')
+    
     def test_優勢腔(self):
         self.assertEqual(臺灣閩南語羅馬字拼音('gio2').轉白話字數字調(), 'gio2')
         self.assertEqual(臺灣閩南語羅馬字拼音('au3').轉白話字數字調(), 'au3')
-        # ó͘, ō͘,
-        self.assertEqual(臺灣閩南語羅馬字拼音('hoo7').轉白話字數字調(), 'ho͘7')
         self.assertEqual(臺灣閩南語羅馬字拼音('ainn7').轉白話字數字調(), 'aiⁿ7')
         self.assertEqual(臺灣閩南語羅馬字拼音('hah8').轉白話字數字調(), 'hah8')
 
