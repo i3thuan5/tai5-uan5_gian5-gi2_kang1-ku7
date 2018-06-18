5e5ef3669d3055e545a12ced83b1785c25dab457
fafoy <ihcaoe@gmail.com>
2018-06-18 10:55:08 +0800

    實作了

--- 試驗/辭典/Test辭典集單元試驗.py
+++ 試驗/辭典/Test辭典集單元試驗.py
@@ -1,16 +1,12 @@
 # -*- coding: utf-8 -*-
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
-from 臺灣言語工具.基本物件.詞 import 詞
-from 臺灣言語工具.解析整理.解析錯誤 import 解析錯誤
-from 臺灣言語工具.解析整理.參數錯誤 import 參數錯誤
-from 臺灣言語工具.解析整理.物件譀鏡 import 物件譀鏡
-from os import remove
-from unittest.mock import patch, call
 from 臺灣言語工具.辭典.型音辭典 import 型音辭典
 from 臺灣言語工具.辭典.現掀辭典 import 現掀辭典
+from 臺灣言語工具.辭典.辭典集 import 辭典集
+from unittest.case import TestCase
 
 
-class 辭典集單元試驗:
+class 辭典集單元試驗(TestCase):
 
     def test_無仝詞典攏愛揣出來(self):
         it = 型音辭典(4)
@@ -19,12 +15,12 @@ class 辭典集單元試驗:
         ji.加詞(拆文分析器.對齊詞物件('你好', 'li2-ho2'))
         bue = 辭典集(it, ji)
         self.assertEqual(
-            bue.查詞(拆文分析器.建立詞物件('li2')),
+            bue.查詞(拆文分析器.建立詞物件('li2-ho2-bo5')),
             [
                 {拆文分析器.對齊詞物件('你', 'li2')},
                 {拆文分析器.對齊詞物件('你好', 'li2-ho2')},
                 set(),
-                {self.詞物件}
+                set(),
             ]
         )
 
