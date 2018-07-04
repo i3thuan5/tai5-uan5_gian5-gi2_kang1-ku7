6461689841a79b112fdebbaaf4927ca0626e2bde
fafoy <ihcaoe@gmail.com>
2018-07-04 09:54:36 +0800

    加拼音字元試驗

--- 臺灣言語工具/基本物件/公用變數.py
+++ 臺灣言語工具/基本物件/公用變數.py
@@ -45,7 +45,6 @@ from 臺灣言語工具.音標系統.閩南語.臺灣閩南語羅馬字拼音轉
 組字式符號 = '⿰⿱⿲⿳⿴⿵⿶⿷⿸⿹⿺⿻⿿'
 
 # Ll　小寫， Lu　大寫， Md　數字， Mn　有調號英文，Lo　其他, So 組字式符號…
-# Lm　注音聲，　Sk　輕聲,　Sm　數學,
 _統一碼羅馬字類 = {'Ll', 'Lu', 'Mn'}
 統一碼漢字佮組字式類 = {'Lo', 'So'}
 
@@ -54,7 +53,10 @@ def 敢是拼音字元(字元, 種類=None):
     if 字元 is None:
         return False
     if 種類 is None:
-        種類=unicodedata.category(字元)
+        try:
+            種類 = unicodedata.category(字元)
+        except TypeError:
+            return False
     return 種類 in _統一碼羅馬字類 or 字元 in ['ⁿ', "'", '_', ]
 
 
