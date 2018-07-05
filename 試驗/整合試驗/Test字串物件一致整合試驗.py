1e6fe722f4d983440a17c594579cd77665909672
a8568730 <a8568730@hotmail.com>
2018-07-05 10:22:53 +0800

    整合試驗 ka 試驗

--- 試驗/整合試驗/Test字串物件一致整合試驗.py
+++ 試驗/整合試驗/Test字串物件一致整合試驗.py
@@ -1,43 +1,74 @@
 # -*- coding: utf-8 -*-
-from unittest.case import TestCase
+from unittest.case import TestCase, skip
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
 
 
 class 物件轉字串整合試驗(TestCase):
+    # 原本字串生按怎就照伊原本--的顯示
 
     def tearDown(self):
         物件 = 拆文分析器.建立句物件(self.語句)
         self.assertEqual(物件.看語句(), self.語句)
 
     def test_全羅(self):
-        self.語句 = 'lia̍h suí-ti!'
+        self.語句 = 'Lia̍h suí-ti'
 
-    def test_漢羅(self):
-        self.語句 = '欲lia̍h-ti!'
-
-    def test_全型標點符號(self):
-        self.語句 = 'ti！'
-    
-    def test_漢字輕聲(self):
-        self.語句 = '--啊'
-
-    def test_羅馬字輕聲(self):
+    def test_全羅輕聲(self):
         self.語句 = '--ah'
 
-    def test_漢字濟字輕聲(self):
-        self.語句 = '緊--出-來'
-
-    def test_羅馬字濟字輕聲(self):
+    def test_全羅濟字輕聲(self):
         self.語句 = 'Kín--tshut-lâi'
 
-    def test_輕聲前有輕聲(self):
+    def test_全羅輕聲前有輕聲(self):
         self.語句 = 'Kín --tshut-lâi'
-    
-    def test_漢字連續輕聲(self):
-        self.語句 = '緊--出-來--啦'
-        
-    def test_羅馬字連續輕聲(self):
+
+    def test_全羅連續輕聲(self):
         self.語句 = 'Kín --tshut-lâi --lah'
-        
-    def test_粗坯的輕聲(self):
+
+    def test_全羅粗坯的輕聲(self):
         self.語句 = 'Kin2-0tshut-lai5-0ah'
+
+    def test_全漢(self):
+        self.語句 = '欲 掠豬'
+
+    def test_全漢無空白(self):
+        self.語句 = '欲掠豬'
+
+    def test_全漢輕聲(self):
+        self.語句 = '--啊'
+
+    def test_全漢濟字輕聲(self):
+        self.語句 = '緊--出-來'
+
+    def test_全漢連續輕聲(self):
+        self.語句 = '緊--出-來--啦'
+
+    @skip('分析器閣有問題')
+    def test_漢字濟字輕聲混合201802p13(self):
+        # = 緊--出-來,好--無
+        self.語句 = '想--起-來就ê驚'
+
+    @skip('分析器閣有問題')
+    def test_漢字濟字輕聲混合201802p13無空白(self):
+        # = 緊--出-來,好--無
+        self.語句 = '想--起-來就 ê 驚'
+
+    @skip('分析器閣有問題')
+    def test_bongpo_201804p25(self):
+        self.語句 = '調--來ê兵仔'
+
+    @skip('分析器閣有問題')
+    def test_bongpo_201804p25無空白(self):
+        self.語句 = '調--來 ê 兵仔'
+
+    def test_漢羅(self):
+        self.語句 = '欲 lia̍h-ti'
+
+    def test_漢羅無空白(self):
+        self.語句 = '欲lia̍h-ti'
+
+    def test_半形標點符號(self):
+        self.語句 = 'ti!'
+
+    def test_全形標點符號(self):
+        self.語句 = 'ti！'
