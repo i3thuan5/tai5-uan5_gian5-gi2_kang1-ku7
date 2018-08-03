4b3e99248c7d51dc4f3d9adef5282521905d6ed0
fafoy <ihcaoe@gmail.com>
2018-08-03 21:19:13 +0800

    Sing ka phinn-hu-im tshi-giam.

--- 試驗/羅馬字/台語/Test新白話字單元試驗.py
+++ 試驗/羅馬字/台語/Test新白話字單元試驗.py
@@ -13,3 +13,18 @@ class 新白話字單元試驗(TestCase):
 
     def test_違法(self):
         self.assertIsNone(新白話字('cō͘').音標)
+
+    def test_鼻聲母袂使配陽韻(self):
+        self.assertIsNone(新白話字('nong').音標)
+
+    def test_鼻聲母袂使配入聲(self):
+        self.assertIsNone(新白話字('mit').音標)
+
+    def test_鼻聲母免寫鼻化韻(self):
+        self.assertIsNone(新白話字('nuann').音標)
+
+    def test_鼻聲母會當韻化輔音(self):
+        self.assertIsNotNone(新白話字('mn̂g').音標)
+
+    def test_濁聲母韻袂使配鼻化韻(self):
+        self.assertIsNone(新白話字('buann').音標)
