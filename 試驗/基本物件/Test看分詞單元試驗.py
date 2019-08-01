30a089bac0331e381911736353dac4536c7c112a
a8568730 <a8568730@hotmail.com>
2019-08-01 11:51:35 +0800

    Ka tshi-giam.

---
+++ 試驗/基本物件/Test看分詞單元試驗.py
@@ -0,0 +1,47 @@
+from unittest.case import TestCase
+from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
+from 臺灣言語工具.基本物件.集 import 集
+from 臺灣言語工具.解析整理.解析錯誤 import 解析錯誤
+from 臺灣言語工具.解析整理.型態錯誤 import 型態錯誤
+
+
+class 看分詞單元試驗(TestCase):
+
+    def test_漢羅對照(self):
+        書寫1 = '豬仔媠'
+        書寫2 = 'ti-á suí'
+        self.assertEqual(
+            拆文分析器.建立句物件(書寫1, 書寫2).看分詞(),
+            '豬-仔｜ti-á 媠｜suí'
+        )
+
+    def test_漢羅OK(self):
+        書寫1 = '豬á媠'
+        書寫2 = 'ti-á suí'
+        self.assertEqual(
+            拆文分析器.建立句物件(書寫1, 書寫2).看分詞(),
+            '豬-á｜ti-á 媠｜suí'
+        )
+
+    def test_詞頭輕聲(self):
+        書寫1 = '媠--啦'
+        書寫2 = 'suí --lah'
+        self.assertEqual(
+            拆文分析器.建立句物件(書寫1, 書寫2).看分詞(),
+            '媠｜suí --啦｜--lah'
+        )
+
+    def test_詞中輕聲(self):
+        書寫1 = '媠--ê'
+        書寫2 = 'suí--ê'
+        self.assertEqual(
+            拆文分析器.建立句物件(書寫1, 書寫2).看分詞(),
+            '媠--ê｜suí--ê'
+        )
+
+    def test_一種書寫(self):
+        書寫 = 'ti-á suí'
+        self.assertEqual(
+            拆文分析器.建立句物件(書寫).看分詞(),
+            'ti-á suí'
+        )
