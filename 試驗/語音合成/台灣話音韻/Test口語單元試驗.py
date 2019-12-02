8b9a44fbdc4236cdc563609aab5925134d6ef932
Fafofafoy <ihcaoe@gmail.com>
2019-12-02 22:30:06 +0800

    Sit-tsok it-puann e ku

--- 試驗/語音合成/台灣話音韻/Test口語單元試驗.py
+++ 試驗/語音合成/台灣話音韻/Test口語單元試驗.py
@@ -2,17 +2,15 @@
 from unittest.case import TestCase
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
 from 臺灣言語工具.音標系統.閩南語.臺灣閩南語羅馬字拼音 import 臺灣閩南語羅馬字拼音
-from 臺灣言語工具.語音合成.閩南語音韻規則 import 閩南語音韻規則
+from 臺灣言語工具.語音合成 import 台灣話口語規則
 
 
 class 口語單元試驗(TestCase):
     def tearDown(self):
         句物件 = (
             拆文分析器.對齊句物件(self.漢字, self.臺羅)
-            .轉音(臺灣閩南語羅馬字拼音)
-            .轉音(臺灣閩南語羅馬字拼音, 函式='音值')
         )
-        self.assertEqual(台灣話口語規則.走(句物件).看語句(), self.答案)
+        self.assertEqual(台灣話口語規則(句物件).看音(), self.答案)
 
     def test_一句(self):
         self.漢字 = '我愛媠媠'
