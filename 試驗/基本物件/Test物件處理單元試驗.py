7d6060164610eace7719ffc45a633483dafcb472
Tira <hong@ithuan.tw>
2023-09-15 09:13:24 +0800

    修試驗。

--- 試驗/基本物件/Test物件處理單元試驗.py
+++ 試驗/基本物件/Test物件處理單元試驗.py
@@ -48,12 +48,6 @@ class 物件處理單元試驗(TestCase):
         self.物件.揀(語言模型揀集內組, 語言模型=語言模型)
         揀mock.assert_called_once_with(語言模型=語言模型, 物件=self.物件)
 
-#     @patch('臺灣言語工具.斷詞.語言模型揀集內組.語言模型揀集內組.揀')
-#     def test_KenLM語言模型揀集內組(self, 揀mock):
-#         語言模型 = KenLM語言模型()
-#         self.物件.揀(語言模型揀集內組, 語言模型)
-#         揀mock.assert_called_once_with(語言模型, 物件=self.物件)
-
     @patch('臺灣言語工具.斷詞.拄好長度辭典揣詞.拄好長度辭典揣詞.揣詞')
     def test_常用辭典揣詞(self, 揣詞mock):
         辭典 = 型音辭典(2)
@@ -72,26 +66,9 @@ class 物件處理單元試驗(TestCase):
         self.物件.揣詞(尾字辭典揣詞(上長詞優先辭典揣詞), 辭典)
         揣詞mock.assert_called_once_with(辭典, 物件=self.物件)
 
-    @patch('臺灣言語工具.斷詞.中研院.斷詞用戶端.斷詞用戶端.斷詞')
-    def test_中研院斷詞(self, 斷詞mock):
-        self.物件.斷詞(斷詞用戶端())
-        斷詞mock.assert_called_once_with(物件=self.物件)
-
     @patch('臺灣言語工具.斷詞.辭典語言模型斷詞.辭典語言模型斷詞.斷詞')
     def test_辭典語言模型斷詞(self, 斷詞mock):
         辭典 = 現掀辭典(2)
         語言模型 = 實際語言模型(1)
         self.物件.斷詞(辭典語言模型斷詞, 辭典, 語言模型)
         斷詞mock.assert_called_once_with(辭典, 語言模型, 物件=self.物件)
-
-    @patch('臺灣言語工具.翻譯.摩西工具.摩西用戶端.摩西用戶端.翻譯')
-    def test_翻譯(self, 翻譯mock):
-        翻譯模組 = 摩西用戶端()
-        self.物件.翻譯(翻譯模組)
-        翻譯mock.assert_called_once_with(物件=self.物件)
-
-    @patch('臺灣言語工具.翻譯.斷詞斷字翻譯.斷詞斷字翻譯.翻譯')
-    def test_斷詞斷字翻譯(self, 翻譯mock):
-        翻譯模組 = 斷詞斷字翻譯(摩西用戶端(), 摩西用戶端())
-        self.物件.翻譯(翻譯模組)
-        翻譯mock.assert_called_once_with(物件=self.物件)
